4c11e226780e77c39f33785ba77b88fd2540273f
Glitch (dust-holy-cafe) <none>
2020-09-25 08:42:47 +0000

    💃🚄 Checkpoint ./ayarlar.json:22869661/131 ./komutlar/yardım.js:22869661/1755

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,34 +5,35 @@ exports.run = (client, message) => {
 const EmbedFwhyCode = new Discord.MessageEmbed()
 
 .setColor("RANDOM")
-.setTitle("**▬▬▬▬▬▬[** ©️ **FwhyCode Bot** ©️ **]▬▬▬▬▬▬**  \n\n> :floppy_disk: `!istatistik` **Botun İstatistiklerine Bakarsınız!** \n> :floppy_disk: **Prefixim:** `!`")
+.setTitle("**▬▬▬▬▬▬[** ©️ **Eğlence Bot (V12)** ©️ **]▬▬▬▬▬▬**  \n\n> :floppy_disk: `!istatistik` **Botun istatistik bilgilerini açar.** \n> :floppy_disk: **Şuanda kullanılan prefix** `!`")
 .setThumbnail("https://cdn.discordapp.com/attachments/655459488236568597/655887650494087178/loading_1.gif")
 .setDescription(`
 
 ▬▬▬▬▬▬[ :closed_lock_with_key: **Eğlence Komutları** :closed_lock_with_key: ]▬▬▬▬▬▬
 
-> **» !aduketçek :** aduket çekersiniz.
-> **» !balık-tut :** Balık Tutarsınız.
-> **» !beşlik :** Etiketlediğiniz Kişiyle Beşlik Çakarsınız.
-> **» !ara155 :** Polisi Arar (ciddiye almayın).
-> **» !efkarım :** Bot Efkarınızı ölçer.
-> **» !ejderha-yazı :** Ejderha Logosu Yaparsınız.
-> **» !espri :** Bot Espri yapar.
-> **» !hesapla :** Bot Belirtilen işlemi yapar.
-> **» !kartopu :** Etiketlediğiniz Kişiye KarTopu Atarsınız.
-> **» !kaç-cm :** Bot Malafatını Söyler.
-> **» !kralol :** Kral Olursunuz.
-> **» !tokat :** Bot Belirtilen kişiyi, Tokatlar!.
-> **» !yazıtura :** Bot Yazı-Tura atar.
-> **» !yılbaşı :** Yılbaşının kutlanmasına kaç gün kaç saat kaç dakika kaç saniye olduğunu gösterir.
-> **» !öp :** İstediğiniz kişiyi öpersiniz..
-> **» !şekerye :** Şeker Yersiniz!.
+> **» !aduketçek :** Etiketlediğiniz kişiye aduket çekersiniz.
+> **» !balık-tut :** Denizde balık tutarsınız.
+> **» !beşlik :** Etiketlediğiniz kişiyle beşlik çakarsınız.
+> **» !ara155 :** Polisi aramanıza yarar.
+> **» !efkarım :** Şu anda olan efkarınızı ölçersiniz.
+> **» !ejderha-yazı :** Ejderha temalı logo üretirsiniz.
+> **» !espri :** Bot sizin için espri yapar.
+> **» !hesapla :** Belirttiğiniz matematik işlemini bot yapar.
+> **» !kartopu :** Etiketlediğiniz kişiye kartopu atarsınız.
+> **» !kaç-cm :** Bot malafat uzunluğunuzu söyler.
+> **» !kralol :** Kral olmanıza yarar.
+> **» !tokat :** Etiketlediğiniz kişiyi tokatlarsınız.
+> **» !yazıtura :** Bot ile yazı-tura oyununu oynarsınız.
+> **» !yılbaşı :** Yılbaşının gelmesine ne kadar yaklaştığınızı gösterir.
+> **» !öp :** Etiketlediğiniz kişiyi öpmenize yarar.
+> **» !şekerye :** Bot size şeker ikram eder.
 
 **▬▬▬▬▬▬▬[** :gear: **Bilgilendirme** :gear: **]▬▬▬▬▬▬▬**
 
 > :dizzy: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin!**
 > :open_file_folder: **Botun Destek Sunucusuna Gelmek İçin [Tıkla!](https://discord.gg/davetlinkiniz)**
 > :round_pushpin: **Komut hakkında detaylı bilgi için: !yardım**
+> :airplane: **Aktif discord.js sürümüm: v12.2.0**
 
 **» Bağlantılar** 
 **[Destek Sunucusu](https://discord.gg/davetlinkiniz)** **•** **[Botun Davet Linki](https://www.discord.com/)** **•** **[Web-Site](https://www.discord.com/)**
