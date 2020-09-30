4d9678ad8d7027a35e708030ebe621dadcfa23d3
Glitch (ukqzneglence) <none>
2020-09-30 15:43:10 +0000

    👳👛 Checkpoint ./komutlar/istatistik.js:16022898/3521

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,40 +1,22 @@
-const Discord = require("discord.js");
-const moment = require("moment");
-const os = require('os');
-require("moment-duration-format");
-exports.run = async (bot, message, args) => {
-   const matador = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-   const istatistikler = new Discord.MessageEmbed()
-  .setColor('RANDOM')
-  .setFooter('Istatistik', bot.user.avatarURL)
-  .addField("» **Botun Sahibi**", "ever_Sitrey!")
-  .addField("»  **Geliştirici** ","<@675349659040088109>")
-  .addField("» **Bellek kullanımı**", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)  
-  .addField("» **Çalışma süresi**", matador)
-  .addField("» **Kullanıcılar**" , bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
-  .addField("» **Sunucular**", bot.guilds.size.toLocaleString(), true)
-  .addField("» **Kanallar**", bot.channels.size.toLocaleString(), true)
-  .addField("» **Discord.JS sürüm**", "v"+Discord.version, true)
-  .addField("» **Node.JS sürüm**", `${process.version}`, true)
-  .addField("» **Ping**", bot.ping+" ms", true)
-  .addField("» **CPU**", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
-  .addField("» **Bit**", `\`${os.arch()}\``, true)
-  .addField("» **İşletim Sistemi**", `\`\`${os.platform()}\`\``) 
-  .addField("**» Bot Davet**", " [Davet Et](LİNK)", )
-  .addField("**» Destek Sunucusu**", " [Sunucumuza Katıl](LİNK)", )
-  .addField("**» Oylama Sayfası**", " [Botu votele](LİNK)", )
- return message.channel.send(istatistikler);
-  };
+const Discord = require('discord.js');
+const moment = require('moment');
+const ayarlar = require('../ayarlar');
+require('moment-duration-format');
+exports.run = async(client, message, args) => {
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [ 'i', 'stats', 'bot', 'botbilgi'],
-  permLevel: 0
-};
+let matador = new Discord.MessageEmbed()
+.setThumbnail(`https://cdn.discordapp.com/attachments/749380170351116290/750088540288712914/B0oBpM.png`)
+.addField("__**Bot Verileri**__", `<a:hypesquad1:750076071721828452> **Toplam Sunucu** **|**  **${client.guilds.cache.size}** \n <a:hypesquad1:750076071721828452> **Toplam Kullanıcı** **|** **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \n <a:hypesquad1:750076071721828452> **Toplam Kanal** **|** **${client.channels.cache.size}**`)
+.addField("__**Bot Geliştiricisi**__", `<a:hypesquad1:750076071721828452> **Bot Sahibi**  <@236173144300191754> \n **Lord Creative#0001** \n\n <a:hypesquad1:750076071721828452>\ **Bot Geliştiricisi**  <@327064201245753344> \n **Bay Ördekcik#0001** \n\n <a:hypesquad1:750076071721828452> **Bot Sahibi**  <@749710702721106020> \n **Xir9999** \n`)
+.addField("__**Sürümler**__", `<a:hypesquad1:750076071721828452> **Discord.js Sürümü** **|**  **v${Discord.version}** \n<a:hypesquad1:750076071721828452> **Node.js Sürümü** **|**  **${process.version}**`)
+.addField("__**Gecikmeler**__", `<a:hypesquad1:750076071721828452> **${client.ws.ping}** ms`,true)
+.addField("**__Müzik Oynatılan__** ", "<a:hypesquad1:750076071721828452> " +client.voice.connections.size + " Sunucu", true)
+    .setImage(`https://i.hizliresim.com/Y7jFCB.png`)
+.setColor("#ffd100")
+message.channel.send(matador)
+}
 
-exports.help = {
-  name: "istatistik",
-  description: "Bot Stats",
-  usage: "istatistik"
-};
+exports.config = {
+name: "botbilgi",
+aliases: []
+}
