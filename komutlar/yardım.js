1237fa0d3339ea974637f43485246dd53de4ba26
Glitch (ukqzneglence) <none>
2020-10-07 12:49:45 +0000

    🐩😀 Checkpoint ./komutlar/yardım.js:16022898/23

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -11,29 +11,29 @@ const Embedmatador = new Discord.MessageEmbed()
 
 ▬▬▬▬▬▬[ :closed_lock_with_key: **Eğlence Komutları** :closed_lock_with_key: ]▬▬▬▬▬▬
 
-  **  !fbi :** FBİ gifi atar.
-  **  !aşkölçer :** Aşkınızı ölçer. 
-**  !aduketçek :** Etiketlediğiniz kişiye aduket çekersiniz.
- **  !ara155 :** Polisi aramanıza yarar.
- **  !ay :** Ay gifi atar.
- **  !balık-tut :** Denizde balık tutarsınız.
- **  !beşlik :** Etiketlediğiniz kişiyle beşlik çakarsınız.
-  **  !dünya :** Dünya gifi atar.
-  **  !efkarım :** Şu anda olan efkarınızı ölçersiniz.
- **  !ejderha-yazı :** Ejderha temalı logo üretirsiniz.
- **  !emojiyazı :** Yazdığınız yazıları emoji halinde yazar.
- **  !espri :** Bot sizin için espri yapar.
- **  !hesapla :** Belirttiğiniz matematik işlemini bot yapar.
- **  !kartopu :** Etiketlediğiniz kişiye kartopu atarsınız.
-    **  !kaç-cm :** Bot malafat uzunluğunuzu söyler.
-    **  !kralol :** Kral olmanıza yarar.
- **  !slots :** Slot oynarsınız.
-    **  !taksimdayı :** Taksim Dayı gifi atar.
-    **  !tokat :** Etiketlediğiniz kişiyi tokatlarsınız.
- **  !yazı-tura :** Bot ile yazı-tura oyununu oynarsınız.
- **  !yılbaşı :** Yılbaşının gelmesine ne kadar yaklaştığınızı gösterir.
-    **  !öp :** Etiketlediğiniz kişiyi öpmenize yarar.
- **  !şekerye :** Bot size şeker ikram eder.
+  **»  !fbi :** FBİ gifi atar.
+  **»  !aşkölçer :** Aşkınızı ölçer. 
+**»  !aduketçek :** Etiketlediğiniz kişiye aduket çekersiniz.
+ **»  !ara155 :** Polisi aramanıza yarar.
+ **»  !ay :** Ay gifi atar.
+ **»  !balık-tut :** Denizde balık tutarsınız.
+ **»  !beşlik :** Etiketlediğiniz kişiyle beşlik çakarsınız.
+  **»  !dünya :** Dünya gifi atar.
+  **»  !efkarım :** Şu anda olan efkarınızı ölçersiniz.
+ **»  !ejderha-yazı :** Ejderha temalı logo üretirsiniz.
+ **»  !emojiyazı :** Yazdığınız yazıları emoji halinde yazar.
+ **»  !espri :** Bot sizin için espri yapar.
+ **»  !hesapla :** Belirttiğiniz matematik işlemini bot yapar.
+ **»  !kartopu :** Etiketlediğiniz kişiye kartopu atarsınız.
+    **»  !kaç-cm :** Bot malafat uzunluğunuzu söyler.
+    **»  !kralol :** Kral olmanıza yarar.
+ **»  !slots :** Slot oynarsınız.
+    **»  !taksimdayı :** Taksim Dayı gifi atar.
+    **»  !tokat :** Etiketlediğiniz kişiyi tokatlarsınız.
+ **»  !yazı-tura :** Bot ile yazı-tura oyununu oynarsınız.
+ **»  !yılbaşı :** Yılbaşının gelmesine ne kadar yaklaştığınızı gösterir.
+    **»  !öp :** Etiketlediğiniz kişiyi öpmenize yarar.
+ **»  !şekerye :** Bot size şeker ikram eder.
 
 **▬▬▬▬▬▬▬[** :gear: **Bilgilendirme** :gear: **]▬▬▬▬▬▬▬**
 
