81d547740e0c7541c35146c1c95e7f0d0255488f
Glitch (ukqzneglence) <none>
2020-12-10 06:57:30 +0000

    🌂👼 Checkpoint ./komutlar/yardım.js:25134648/36

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -25,14 +25,14 @@ const Embedmatador = new Discord.MessageEmbed()
  **»  *espri :** Bot sizin için espri yapar.
  **»  *hesapla :** Belirttiğiniz matematik işlemini bot yapar.
  **»  *kartopu :** Etiketlediğiniz kişiye kartopu atarsınız.
-    **»  *kaç-cm :** Bot malafat uzunluğunuzu söyler.
-    **»  *kralol :** Kral olmanıza yarar.
+    **»  *kaç-cm :** Bot malafat uzunluğunuzu söyler.(ironi)
+    **»  *kralol :** Kral olmanıza yarar.(ironi)
  **»  *slots :** Slot oynarsınız.
     **»  *taksimdayı :** Taksim Dayı gifi atar.
-    **» *tokat :** Etiketlediğiniz kişiyi tokatlarsınız.
+    **» *tokat :** Etiketlediğiniz kişiyi tokatlarsınız.(ironi)
  **»  *yazı-tura :** Bot ile yazı-tura oyununu oynarsınız.
  **»  *yılbaşı :** Yılbaşının gelmesine ne kadar yaklaştığınızı gösterir.
-    **»  *öp :** Etiketlediğiniz kişiyi öpmenize yarar.
+    **»  *öp :** Etiketlediğiniz kişiyi öpmenize yarar.(ironi)
  **»  *şekerye :** Bot size şeker ikram eder.
  **»  *atatürk :** Atamızı saygıyla anıyoruz.
  **»  *mcskin :** Belirttiniz kişin minecraft skinini(karakterini) atar.
@@ -45,7 +45,7 @@ const Embedmatador = new Discord.MessageEmbed()
 
 :Kurucum: TREFAX#0038**
 :Yardım için kurucuma yazınız kurucum: TREFAX#0038**
-  :round_pushpin: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin! Fikirleriniz için lütfen kurucuma yazın kurucum: TREFAX#0038**
+  :round_pushpin: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin! Fikirleriniz için lütfen kurucuya yazın kurucum: TREFAX#0038**
 :Botu sunucunuza davet etmek için lütfen kurucuya yazınız kurucu: TREFAX#0038
   :dizzy: **Komut hakkında detaylı bilgi için: *yardım**
   :airplane: **Aktif discord.js sürümüm: v12.2.0**
