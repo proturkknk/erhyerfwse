3a1b34fd45e451d3eaf404e8ef7e5220c2d6cf2e
Glitch (ukqzneglence) <none>
2020-12-30 10:04:24 +0000

    💃🍯 Checkpoint ./events/message.js:25134648/4748 ./komutlar/bakım.js:25134648/2487 ./util/eventLoader.js:25134648/2022

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -2,4 +2,3 @@ const reqEvent = (event) => require(`../events/${event}`);
 module.exports = client => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('message', reqEvent('message'));
-};
