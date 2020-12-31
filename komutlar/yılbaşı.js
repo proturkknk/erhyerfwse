ec60124eff778d01a60bcabd0b964152b090ef87
Glitch (ukqzneglence) <none>
2020-12-31 10:55:24 +0000

    🌮📻 Checkpoint ./komutlar/yılbaşı.js:25692167/1143

--- komutlar/yılbaşı.js
+++ komutlar/yılbaşı.js
@@ -1,15 +1,15 @@
 const Discord = require ("discord.js");
 const db = require("quick.db");
 const ms = require("parse-ms");
-const moment = require('moment-timezone')
 
 exports.run = async (client, message, args) => {
 
-  let yilbasi = new Date("2020-12-31 24:00:00");
-  const day = moment().tz('Turkey').format('DD')
+  let yilbasi = new Date("2020-12-31 21:01:00");
+  let zaman = ms(yilbasi - Date.now('Turkey'));
+  if(zaman.seconds <= 0 && zaman.minutes <= 0 && zaman.hours <= 0) return message.reply('Zaten 2021 deyiz :)')
 
 return message.channel.send(
-    `:label: **Yılbaşının kutlanmasına Dair Bilgiler Aşağıda;** \n\n> Gün: **${day}** \n> Saat: **${day}** \n> Dakika: **${day}** \n\n:white_check_mark: **Yılbaşının kutlanmasına Bu Kadar Zaman Kaldı! Mutlu Yıllar!
+    `:label: **Yılbaşının kutlanmasına Dair Bilgiler Aşağıda;** \n\n> Gün: **${zaman.days}** \n> Saat: **${zaman.hours}** \n> Dakika: **${zaman.minutes}** \n\n:white_check_mark: **Yılbaşının kutlanmasına Bu Kadar Zaman Kaldı! Mutlu Yıllar!
     2021 Hoşgeldin!...**`
   );
 };
