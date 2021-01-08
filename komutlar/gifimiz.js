542e45b60d39f2bafa30f982c8f3f13d746b316f
Glitch (ukqzneglence) <none>
2021-01-08 13:44:42 +0000

    💝🎮 Checkpoint ./komutlar/gifimiz.js:25134648/294

--- komutlar/gifimiz.js
+++ komutlar/gifimiz.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let replies = ["https://media.giphy.com/media/LZfH1caaZveK3KhXBb/giphy.gif"];
+    let replies = ["https://media.giphy.com/media/LZfH1caaZveK3KhXBb/giphy.gif,https://media.giphy.com/media/bjHZUMEGO3bVJzOqm9/giphy.gif"];
 
     let result = Math.floor((Math.random() * replies.length));
 
     let gifembed = new Discord.MessageEmbed()
         .setTitle("U-Bot gifimiz!")
         .setColor("#FF69B4")
-        .setFooter(`Beğendinizmi? :)${message.author.tag} `, message.author.avatarURL)
-        .setImage("https://media.giphy.com/media/LZfH1caaZveK3KhXBb/giphy.gif")
+        .setFooter(`Beğendinizmi? :) ${message.author.tag} `, message.author.avatarURL)
+        .setImage("https://media.giphy.com/media/bjHZUMEGO3bVJzOqm9/giphy.gif")
 
     message.channel.send(gifembed);
 };
