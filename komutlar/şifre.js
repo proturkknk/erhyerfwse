cd3fd64ff1387500082295b9c4790f372fdd2cad
Glitch (ukqzneglence) <none>
2021-01-15 11:56:10 +0000

    🏡❄️ Checkpoint ./komutlar/şifre.js:25134648/83:25692167/242

--- komutlar/şifre.js
+++ komutlar/şifre.js
@@ -4,8 +4,9 @@ const generator = require('generate-password');
 
 exports.run = function(client, message, args,params) {
     var uzunluk = args.slice(0).join(' ');
-  
+  const limit = 50
   if (isNaN(uzunluk)) return message.reply(" :x: Geçersiz! Lütfen Örneğe Bakınız; ``+şifre 10`` Hata sebebi: ``Geçersiz Sayı``")
+  if(uzunluk > limit) return message.reply(`:x: Hata! Max sınır: ${limit}`)
     if (!uzunluk) return message.channel.send(' :x: Geçersiz! Lütfen Örneğe Bakınız; ``+şifre 10``')
     var password = generator.generate({
         length: uzunluk,
@@ -13,7 +14,7 @@ exports.run = function(client, message, args,params) {
     })
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 30);
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     message.author.send(password);
   if (message.channel.type !== 'dm') {
     message.channel.send(':white_check_mark: Şifreniz Özel Olarak Size İletildi!') }
