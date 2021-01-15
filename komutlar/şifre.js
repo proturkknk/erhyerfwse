9d64f04f33916947463ff5d6328e058c04fbb200
Glitch (ukqzneglence) <none>
2021-01-15 11:45:07 +0000

    🎐👫 Checkpoint ./komutlar/şifre.js:25134648/102

--- komutlar/şifre.js
+++ komutlar/şifre.js
@@ -5,18 +5,18 @@ const generator = require('generate-password');
 exports.run = function(client, message, args,params) {
     var uzunluk = args.slice(0).join(' ');
   
-  if (isNaN(uzunluk)) return message.reply(":ak: Geçersiz! Lütfen Örneğe Bakınız; ``+şifre 10`` Hata sebebi: ``Geçersiz Sayı``")
-    if (!uzunluk) return message.channel.send(':ak: Geçersiz! Lütfen Örneğe Bakınız; ``+şifre 10``')
+  if (isNaN(uzunluk)) return message.reply(" :x: Geçersiz! Lütfen Örneğe Bakınız; ``+şifre 10`` Hata sebebi: ``Geçersiz Sayı``")
+    if (!uzunluk) return message.channel.send(' :x: Geçersiz! Lütfen Örneğe Bakınız; ``+şifre 10``')
     var password = generator.generate({
         length: uzunluk,
         numbers: true,
     })
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 30);
     message.author.send(password);
   if (message.channel.type !== 'dm') {
-    message.channel.send(':akA: Heyy! Şifreniz Özel Olarak Size İletildi!') }
+    message.channel.send(':white_check_mark: Şifreniz Özel Olarak Size İletildi!') }
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
