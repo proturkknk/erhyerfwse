102dc53baece13cf8c6b2ae504087e68db74dcf9
Glitch (ukqzneglence) <none>
2021-01-17 10:20:26 +0000

    🙋🙌 Checkpoint ./komutlar/yapımcım.js:25134648/262 ./komutlar/ekip.js:25134648/547

---
+++ komutlar/ekip.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+  const rexus = new Discord.MessageEmbed()
+    .setColor("#00ee00")
+    .setDescription("> U-Bot Ekibimiz: ! TREFAX#0362, ! kerem98#7884,! 𝓧𝔁𝓟𝓻𝓸𝓟𝓵𝓪𝔂𝔃𝔁𝓧 !#1611")
+    .setFooter("<@696365117063036986>")
+  message.channel.send(rexus);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "ekibimiz",
+  description: "",
+  usage: ""
+};
