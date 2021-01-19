66508148c41d9089518e79e227f61d96db074534
Glitch (ukqzneglence) <none>
2021-01-19 06:41:35 +0000

    📠❤️ Checkpoint ./komutlar/sa-as.js:25134648/1703 ./komutlar/kedi.js:25134648/831

--- komutlar/sa-as.js
+++ komutlar/sa-as.js
@@ -2,36 +2,32 @@ const db = require('quick.db')
 const Discord = require('discord.js')
 
 exports.run = async (bot, message, args) => {
-
-if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Bu Komutu Kullanmak İçin **Yönetici** Yetkisine Sahip Olmalısın!");
-
-if (!args[0]) return message.channel.send('Sa-as yazısını açmak için; k.`sa-as aç veya kapat`')
-
-if (args[0] == 'aç') {
   
-db.set(`saas_${message.guild.id}`, 'açık')
+  if (!args[0]) return message.channel.send('<:ak:617145990742278144> Sa-as yazısını açmak için; `!!sa-as aç veya kapat`')
   
-message.channel.send(`**Sa-As Sistemi Açıldı** `)
-}
-  
-if (args[0] == 'kapat') {
-  
-db.set(`saas_${message.guild.id}`, 'kapali')
-  
-message.channel.send(`**Sa-As Sistemi Kapandı** `)
-}
+  if (args[0] == 'aç') {
+    db.set(`ss_${message.guild.id}`, 'açık')
+      message.channel.send(`Başarıyla botun \`Aleyküm selam\` yazmasını açtınız., Artık bot \`sa\` yazıldığında cevap verecek.`)
+    
+  }
+  if (args[0] == 'kapat') {
+    db.delete(`ss_${message.guild.id}`, 'kapat')
+      message.channel.send(` Başarıyla \`Aleyküm selam\` yazmasını kapattınız, Artık bot \`sa\` yazıldığında cevap vermeyecek.`)
+    
+  }
 
 }
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
-  permLevel: 0
+  aliases: ['sa','as'],
+  kategori: 'ayarlar',
+  permLevel: 3
 };
 
 exports.help = {
   name: 'sa-as',
   description: 'Selamün aleyküm, Aleyküm selam',
-  usage: 'sa-as'
+  usage: '!!sa-as'
 };
