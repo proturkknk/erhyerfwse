6f658209346323a1c3d4c59d2155d6281d4f9cd6
Glitch (ukqzneglence) <none>
2021-02-01 14:30:38 +0000

    💵🏙 Checkpoint ./komutlar/atatürk-sözleri.js:25134648/57 ./.glitch-assets:25134648/523

--- komutlar/atatürk-sözleri.js
+++ komutlar/atatürk-sözleri.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 exports.run = (client, message, params) => {
     var sans = ["Yurtta sulh, cihanda sulh!", "Ey yükselen yeni nesil, istikbal sizindir. Cumhuriyet'i biz kurduk, O'nu yükseltecek ve sürdürecek sizlersiniz.", "Ne mutlu Türküm diyene!", "Hayatta en hakiki mürşit ilimdir.", "Egemenlik verilmez, alınır.", "Şuna inanmak gerekir ki, dünya yüzünde gördüğümüz her şey kadının eseridir.", "Hayatı ve özgürlüğü için ölümü göze alan bir millet asla yenilmez", "Bir ulus sanattan ve sanatçıdan yoksunsa, tam bir hayata sahip olamaz.", "Vuruşa Vuruşa Ölmeyi Tercih Ederiz!"];
     var sonuc = sans[Math.floor((Math.random() * sans.length))];
-    const embed = new Discord.RichEmbed()
+    const embed = new Discord. MessageEmbed() 
     .addField(`Atatürk sözleri: `, `${sonuc}`)
     .setTimestamp()
-    return message.channel.sendEmbed(embed);
+    return message.channel.MessageEmbed();
 };
 
 exports.conf = {
