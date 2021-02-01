44b8ec5c8ac3b6fd195e7c30a9c7c026fcf24a07
Glitch (ukqzneglence) <none>
2021-02-01 00:13:24 +0000

    💂🏰 Checkpoint

---
+++ komutlar/alkış.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+exports.run = function(client, message, args) {
+  
+  const alkış = new Discord.MessageEmbed()
+  .setDescription("**Bravooo!!**")
+  .setColor("#313131")
+  .setImage("https://thumbs.gfycat.com/WarpedAdmiredCormorant-size_restricted.gif")
+
+  message.channel.send(alkış);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['Alkış'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'alkış',
+  description: 'Bot Alkışlar',
+  usage: 'alkış'
+  };
