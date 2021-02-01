2b66b8b946d184f88071201e674de204807c5a85
Glitch (ukqzneglence) <none>
2021-02-01 14:53:44 +0000

    💺🍶 Checkpoint ./komutlar/çekilişyap.js:25134648/825 ./komutlar/polis.js:25134648/1063

---
+++ komutlar/polis.js
@@ -0,0 +1,29 @@
+const db = require('quick.db')
+const Discord = require("discord.js")
+exports.run = (client,message) => {
+  let levix;
+  let kanal = "728190172490891275" //kanal id
+  
+    levix = new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setAuthor(message.author.username +  'Karakola Senin Hakkında İhbar Düştü !!! (ironi ciddiye )')
+  .setImage(`http://www.hareketligifler.net/data/media/114/polis-hareketli-resim-0023.gif`)
+  .setDescription(``)
+ message.channel.send(`<@728190172490891275> Karakola Bak Hakkında Biri İhbar Yapmış. (ironi ciddiye almayın) ;(`) 
+
+  client.channels.get(kanal).send(levix)
+  
+    }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'polis',
+  description: 'Polisi Arar (ciddiye almayın)',
+  usage: 'polis'
+};
