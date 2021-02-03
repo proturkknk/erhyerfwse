00dfff87393ae2f37697629ac1ed314ec10965b1
Glitch (ukqzneglence) <none>
2021-02-03 10:53:58 +0000

    🐲👸 Checkpoint ./komutlar/davet.js:25134648/1535

---
+++ komutlar/davet.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+  const motion = new Discord.MessageEmbed()
+    .setColor("#00ee00")
+    .setAuthor(`Motion Bot`, client.user.avatarURL)
+    .addField(
+      `> Neden Biz ?`,
+      `**Çünkü U-BOT Ekibi olarak işimizi en iyi şekilde yaptığımızı düşünüyoruz ve sizide aramızda görmekten mutluluk duyuyoruz. :)**`
+    )
+    .addField(
+      `> Botumuzu Davet Etmek isterseniz`,
+      `[Botu Davet Et!](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8)`
+    )
+    .addField(
+      `> Destek Sunucusuna Katılmak İsterseniz`,
+      `[Destek Sunusu](https://discord.gg/Kekc2pU)`
+    )
+    .addField(
+      `> Kod Paylaşım Sunusuna Katılmak İsterseniz`,
+      `[Kod Paylaşım Sunucumuz](ÇOK YAKINDA!)`
+    )
+    .addField(
+      `> Sitemize Göz Atmak isterseniz`,
+      `[Sitemiz](ÇOK YAKINDA)`
+    )
+
+    .setThumbnail(
+      "https://cdn.discordapp.com/emojis/670167074584395816.gif?v=1"
+    );
+  message.channel.send(motion);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "davet",
+  description: "",
+  usage: ""
+};
