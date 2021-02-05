a8fdb5575c98cf6fbeedc54b946f66754eb7e31c
Glitch (ukqzneglence) <none>
2021-02-05 14:27:58 +0000

    🏏📙 Checkpoint ./komutlar/yardım.js:25134648/16

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,7 +5,7 @@ exports.run = (client, message) => {
 const Embedmatador = new Discord.MessageEmbed()
 
 .setColor("RED")
-.setTitle("**▬▬▬▬▬▬[** ©️ **U-BOT YARDIM KOMUTLARI** ©️ **]▬▬▬▬▬▬**  \n \n **Şuanda kullanılan prefix** `+`")
+.setTitle("**▬▬▬▬▬▬[** ©️ **Xaine Bot YARDIM KOMUTLARI** ©️ **]▬▬▬▬▬▬**  \n \n **Şuanda kullanılan prefix** `+`")
 .setThumbnail("")
 .setDescription(`
 
