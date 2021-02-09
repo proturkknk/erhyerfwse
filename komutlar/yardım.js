4782d1b31d33cb595b046510f40190bbdae9533d
Glitch (ukqzneglence) <none>
2021-02-09 06:54:24 +0000

    🎄💶 Checkpoint ./komutlar/yardım.js:25134648/642

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,23 +5,27 @@ exports.run = (client, message,args) => {
  .setAuthor("Xaine", client.user.avatarURL)
   .setColor('BLACK')
   .setTitle("Xaine") 
- .setURL(https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8 
   .setDescription(
                   
  **+yardım**, ile yardım alabilirsiniz.
-Örnek komut kullanımı: \-ping`
-Botu davet etmek için: `-davet`
+Örnek komut kullanımı: `+ping`
+Botu davet etmek için: `+davet`
+Destek sunucusuna gelmek için: `+davet`
 )
-                  
+    
+ .addField("-moderatör (20", 
+Yetkililerin kullanabileceği komutlar;
+`uyar` `sunucukur`
+              
   .addField("-komutlar (20", 
 herkesin kullanabileceği standart komutlar;
-`avatar` `+davet` `` `yılbaşı` `hesapla` `+havadurumu` `atatürk` `+is` `+bildir` `+korona` `ping` `+saat` `+yapımcım` `token` `+sunucubilgi`  `ekibimiz`  `korona`
+`avatar` `davet` `` `yılbaşı` `hesapla` `+havadurumu` `atatürk` `+is` `+bildir` `+korona` `ping` `saat` `yapımcım` `token` `sunucubilgi`  `ekibimiz`  `korona`
 )
 
  
   .addField("-eğlence (18)", 
 herkesin kullanabileceği eğlence komutları;
-`8ball` `atatürk` `aşkölçer` `balıktut` `duello`  `efkarım` `fbi` `resim` `tersyazı`  `yazıtura`  `öp` `aduketçek`  `ara155`  `ay`   `balık-tut`  `beşlik`  `dünya`  `ejderha-yazı` `emojiyazı`  `espri`  `kartopu`  `kralol`  `slots`  `taksimdayı`  `tokat`  `yazı-tura`  `çıkma-teklifi`  `şekerye`  `düello`  `çekiliş`  `oylama`  `güneş`  `yıldız`  `kar`  `adamasmaca`  `stresçarkı`  ``  ``
+`8ball` `atatürk` `aşkölçer` `balıktut` `duello`  `efkarım` `fbi` `resim` `tersyazı`  `yazıtura`  `öp` `aduketçek`  `ara155`  `ay`   `balık-tut`  `beşlik`  `dünya`  `ejderha-yazı` `emojiyazı`  `espri`  `kartopu`  `kralol`  `slots`  `taksimdayı`  `tokat`  `yazı-tura`  `çıkma-teklifi`  `şekerye`  `düello`  `çekiliş`  `oylama`  `güneş`  `yıldız`  `kar`  `adamasmaca`  `stresçarkı`  `duygulandırıcı-sözler`  `şifre`  `gifimiz`  `zarat`  `nsfw`  `gökyüzü`  `atatürk-sözleri`  `fal`  `boks-makinesi`  `şifre-oluştur`  `ağla`
 )
  
 
@@ -41,6 +45,6 @@ exports.conf = {
 } 
 exports.help = {
   name: "yardım", 
-  description: "Xainon V12  yardım ", 
+  description: "CodeWork V12  yardım ", 
   usage: "yardım" 
 }
