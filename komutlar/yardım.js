bac1949d677a2fec4aa31d8a2d12f7bbb3a3f5b3
Glitch (ukqzneglence) <none>
2021-02-09 07:52:38 +0000

    🌹🏄 Checkpoint ./komutlar/yardım.js:25134648/463

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,35 +1,37 @@
 const Xaine = require('discord.js')
 
 exports.run = (client, message,args) => {
-const  = new message.channel.send.MessageEmbed()
+ const codework1 = new codework.MessageEmbed()
  .setAuthor("Xaine", client.user.avatarURL)
   .setColor('BLACK')
-  .setTitle("Xaine")                
+  .setTitle("Xaine") 
+  .setDescription"(https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8)"
+                  
  "+yardım", "ile yardım alabilirsiniz"
-"Örnek komut kullanımı:" `+ping`
+"Örnek komut kullanımı": `+ping`
 "Botu davet etmek için:" `+davet`
-"Destek sunucusuna gelmek için:" `+davet`
-
+Destek sunucusuna gelmek için: `+davet`
+)
     
  .addField("-moderatör (20", 
-"Yetkililerin kullanabileceği komutlar;"
+Yetkililerin kullanabileceği komutlar;
 `uyar` `sunucukur`
 )
   .addField("-komutlar (20", 
-"herkesin kullanabileceği standart komutlar;"
+herkesin kullanabileceği standart komutlar;
 `avatar` `davet` `` `yılbaşı` `hesapla` `+havadurumu` `atatürk` `+is` `+bildir` `+korona` `ping` `saat` `yapımcım` `token` `sunucubilgi`  `ekibimiz`  `korona`
 )
 
  
   .addField("-eğlence (18)", 
-"herkesin kullanabileceği eğlence komutları;"
+herkesin kullanabileceği eğlence komutları;
 `8ball` `atatürk` `aşkölçer` `balıktut` `duello`  `efkarım` `fbi` `resim` `tersyazı`  `yazıtura`  `öp` `aduketçek`  `ara155`  `ay`   `balık-tut`  `beşlik`  `dünya`  `ejderha-yazı` `emojiyazı`  `espri`  `kartopu`  `kralol`  `slots`  `taksimdayı`  `tokat`  `yazı-tura`  `çıkma-teklifi`  `şekerye`  `düello`  `çekiliş`  `oylama`  `güneş`  `yıldız`  `kar`  `adamasmaca`  `stresçarkı`  `duygulandırıcı-sözler`  `şifre`  `gifimiz`  `zarat`  `nsfw`  `gökyüzü`  `atatürk-sözleri`  `fal`  `boks-makinesi`  `şifre-oluştur`  `ağla`
 )
  
 
 
  
-  ".setFooter(Xaine Bot | Tüm hakları saklıdır.`)"
+  .setFooter(Xaine Bot | Tüm hakları saklıdır.`)
 
  
  message.channel.send(Xaine)
