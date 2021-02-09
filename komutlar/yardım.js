2cb8969719e418758aafa47d9df2f71c407cae03
Glitch (ukqzneglence) <none>
2021-02-09 10:26:31 +0000

    🎩👄 Checkpoint ./komutlar/yardım.js:25134648/608 ./komutlar/yardım2.js:25134648/2606

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,13 +5,12 @@ exports.run = (client, message,args) => {
  .setAuthor("Xaine", client.user.avatarURL)
   .setColor('BLACK')
   .setTitle("Xaine") 
-  .setDescription("[ekleme linki](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8%22)")
-
- .addField("-moderatör (20", " Yetkililerin kullanabileceği komutlar;")
-.addField("-komutlar (20", "  |avatar| |davet| |yılbaşı| |hesapla| |havadurumu| |atatürk| |is| |bildir| |korona| |ping| |saat| |yapımcım| |token| |sunucubilgi|  |ekibimiz|  |korona|)"
+  .setDescription("[davet yazarak tüm davet linklerine ulaşabilirsiniz.](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8%22)")
+ .setDescription("[]
+.addField("-Normal komutlar (20", '` avatar - davet - yılbaşı - hesapla - havadurumu - atatürk - is - bildir - korona - ping - saat - yapımcım - token - sunucubilgi - ekibimiz - korona`')
 
  
-  .addField("-eğlence (18)", "|8ball| atatürk| aşkölçer| balıktut| duello | efkarım| fbi|  tersyazı| yazıtura| öp|aduketçek | ara155|  ay|  |balık-tut|  |beşlik|  |dünya|  |ejderha-yazı| |emojiyazı|  |espri|  |kartopu|  |kralol|  |slots|  |taksimdayı|  |tokat||  |yazı-tura|  |çıkma-teklifi|  |şekerye|  |düello|  |oylama|  |güneş|  |yıldız|  |kar|  |adamasmaca|  |stresçarkı|  |duygulandırıcı-sözler|  |şifre|  |gifimiz|  |zarat|  |nsfw|  |gökyüzü|  |atatürk-sözleri|  |fal|  |boks-makinesi|  |şifre-oluştur|ağla")
+  .addField("-eğlence komutları (18)", '`8ball - atatürk - aşkölçer - balıktut - duello - efkarım - fbi - tersyazı - yazıtura - öp - aduketçek - ara155 - ay - balık-tut - beşlik - dünya - ejderha-yazı - emojiyazı - espri - kartopu - kralol - slots - taksimdayı - tokat - yazı-tura - çıkma-teklifi - şekerye - düello - çekiliş - oylama - güneş - yıldız - kar - adamasmaca - stresçarkı - duygulandırıcı-sözler - şifre - gifimiz - zarat - nsfw - gökyüzü - atatürk-sözleri - fal - boks-makinesi - şifre-oluştur - ağla`')
  
 
 message.channel.send(bok)
