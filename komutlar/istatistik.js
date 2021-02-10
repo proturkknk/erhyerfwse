02f425e37f30d69e9ca95c64b76f12b173cf3519
Glitch (ukqzneglence) <none>
2021-02-10 14:45:53 +0000

    🌈🎮 Checkpoint ./komutlar/istatistik.js:26067249/111

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args) => {
   const istatistikler = new Discord.MessageEmbed()
     .setColor("YELLOW")
     .addField("**__Ping__**",`Mesaj Gecikmesi: ${new Date().getTime() - message.createdTimestamp} ms\n <a:ayar:779332515029581835> Bot Gecikmesi: ${client.ws.ping}ms`, true)
-    .addField("**__Kullanıcı Sayısı__** ", `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`, true)
+    .addField("**__ Kullanıcı Sayısı__** ",`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`, true) 
     .addField("**__Sunucu Sayısı__**", `${client.guilds.cache.size.toLocaleString()}`, true)
     .addField("**__Kanal Sayısı__**", `${client.channels.cache.size.toLocaleString()}`, true)
     .addField("**__Aktiflik__**", `${payidarzaman}`, true)
@@ -27,7 +27,7 @@ exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['is'],
+  aliases: ['istatistik'],
   permLevel: 0
 };
 
