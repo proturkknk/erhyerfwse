0bb53d26a9e7f3d1f6c5779b3360462f16b1dba3
Glitch (ukqzneglence) <none>
2021-02-14 10:58:57 +0000

    ⭐️👼 Checkpoint ./komutlar/yardım.js:26067249/10

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message,args) => {
 .addField("-Normal komutlar (20", '` avatar - davet - yılbaşı - hesapla - havadurumu - atatürk - istatistik - bildir - korona - ping - saat - yapımcım - token - sunucubilgi - ekibimiz - korona`')
 
  
-  .addField("-eğlence komutları (18)", '`8ball - atatürk - aşkölçer - balıktut - duello - efkarım - fbi - tersyazı - yazıtura - öp - aduketçek - ara155 - ay - balık-tut - beşlik - dünya - ejderha-yazı - emojiyazı - espri - kartopu - kralol - slots - taksimdayı - tokat - yazı-tura - çıkma-teklifi - şekerye - düello - çekiliş - oylama - güneş - yıldız - kar - adamasmaca - stresçarkı - duygulandırıcı-söz - şifre - gifimiz - zarat - nsfw - gökyüzü - atatürk-sözleri - fal - boks-makinesi - şifre-oluştur - ağla - masketak`')
+  .addField("-eğlence komutları (18)", '`8ball - atatürk - aşkölçer - balıktut - duello - efkarım - fbi - tersyazı - yazıtura - öp - aduketçek - ara155 - ay - balık-tut - beşlik - dünya - ejderha-yazı - emojiyazı - espri - kartopu - kralol - slots - taksimdayı - tokat - yazı-tura - çıkma-teklifi - şekerye - düello - çekiliş - oylama - güneş - yıldız - kar - adamasmaca - stresçarkı - duygulandırıcı-söz - şifre - gifimiz - zarat - nsfw - gökyüzü - atatürk-sözleri - fal - boks-makinesi - şifre-oluştur - ağla - masketak - sevgili`')
  .setImage(`https://media.giphy.com/media/UDjA5NF3NAlKdHnOQG/giphy.gif`)
 
 message.channel.send(bok)
