00702e56e95deddb478a18dd47a4b7980a7122e7
Glitch (ukqzneglence) <none>
2021-02-14 09:25:58 +0000

    📡😇 Checkpoint ./komutlar/sevgili.js:26067249/829

---
+++ komutlar/sevgili.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    const matador = new Discord.MessageEmbed()
+    .setAuthor('Ne tatlı sevgililer. 😍')
+    .setColor('BLUE')
+    .setTimestamp()
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/2hpqBX8LcDf7tC50St/giphy.gif`)
+    return message.channel.send(matador);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['SEVGİLİ','Sevgili','sevgili'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sevgili',
+  description: 'sevgili.',
+  usage: 'sevgili'
+};
