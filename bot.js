ab8412283346d568733eab50e57c57de92ae3d7d
Glitch (ukqzneglence) <none>
2021-02-15 14:03:16 +0000

    💏🛫 Checkpoint ./bot.js:26067249/6088 ./komutlar/yaz-kazan.js:26067249/7472 ./komutlar/çekiliş.js:26067249/6223 ./komutlar/dürüm.js:26067249/770

--- bot.js
+++ bot.js
@@ -155,3 +155,48 @@ client.on("message", async message => {
   if (command) command.run(client, message, args);
 
 });
+
+client.on("message", async (message , bot) => {
+if (message.content.startsWith("/spotify")) {
+    let user;
+    if (message.mentions.users.first()) {
+      user = message.mentions.users.first();
+    } else {
+      user = message.author;
+    }
+    
+    let convert = require('parse-ms')
+    
+    let status = user.presence.activities[0];
+    
+    if (user.presence.activities.length === 0 || status.name !== "Spotify" && status.type !== "LISTENING") return message.channel.send("Bu kullanıcı Müzik Dinlemiyor.");
+    
+
+      let image = `https://i.scdn.co/image/${status.assets.largeImage.slice(8)}`,
+          url = `https://open.spotify.com/track/${status.syncID}`,
+          name = status.details,
+          artist = status.state,
+          album = status.assets.largeText,
+          timeStart = status.timestamps.start,
+          timeEnd = status.timestamps.end,
+          timeConvert = convert(timeEnd - timeStart);
+      
+      let minutes = timeConvert.minutes < 10 ? `0${timeConvert.minutes}` : timeConvert.minutes;
+      let seconds = timeConvert.seconds < 10 ? `0${timeConvert.seconds}` : timeConvert.seconds;
+      
+      let time = `${minutes}:${seconds}`;
+      
+      const embed = new Discord.MessageEmbed()
+      .setAuthor("Spotify Parça Bilgisi", "https://image.flaticon.com/icons/png/512/2111/2111624.png")
+      .setColor(0x1ED768)
+      .setThumbnail(image)
+      .addField("İsim:", name, true)
+      .addField("Albüm:", album, true)
+      .addField("Artist:", artist, true)
+      .addField("Süre:", time, false)
+      .addField("Spotifyda Dinle!", `[\`${artist} - ${name}\`](${url})`, false)
+      message.channel.send(embed)
+    
+  }
+})
+
