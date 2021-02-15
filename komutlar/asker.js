b1dc059625b7e407962718e84ae7322afb5df563
Glitch (ukqzneglence) <none>
2021-02-15 06:43:54 +0000

    🛥📫 Checkpoint ./komutlar/asker.js:26067249/1279

--- komutlar/asker.js
+++ komutlar/asker.js
@@ -3,21 +3,20 @@ const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message, params) => {
     const matador = new Discord.MessageEmbed()
-    .setAuthor('İşte Kahraman Türk Askerleri! :flag_tr :flag_tr :flag_tr')
+    .setAuthor('İşte Kahraman Türk Askerleri!')
     .setColor('BLUE')
     .setTimestamp()
-    .setDescription('https://media.giphy.com/media/bPBUwqv2gfQKFD0eLG/giphy.gif')
-        .setImage(``)
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/bPBUwqv2gfQKFD0eLG/giphy.gif`)
     return message.channel.send(matador);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['ay','Ay','AY'],
+  aliases: ['ASKER','asker','Asker'],
   permLevel: 0
 };
-
 exports.help = {
   name: 'asker',
   description: 'asker.',
