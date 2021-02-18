553a48cf1e890e9943a5a8e5ce134e25251c1d4d
Glitch (ukqzneglence) <none>
2021-02-18 07:52:31 +0000

    🍬🌋 Checkpoint ./komutlar/kandil.js:26067249/6979

---
+++ komutlar/kandil.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    const matador = new Discord.MessageEmbed()
+    .setAuthor('Kandiliniz Mübarek Olsun. :) <3 🤲 ')
+    .setColor('BLUE')
+    .setTimestamp()
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/RL3OxE3e6aijWo0Vz2/giphy.gif`)
+    return message.channel.send(matador);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['kandil','Kandil','KANDİL'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'kandil',
+  description: 'kandil.',
+  usage: 'kandil'
+};
