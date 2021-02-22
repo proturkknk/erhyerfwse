f222fe9e9b6bda463736b98a834c5f50a569ee67
Glitch (ukqzneglence) <none>
2021-02-22 14:50:50 +0000

    🚙👨 Checkpoint ./komutlar/gol.js:26067249/565

---
+++ komutlar/gol.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+ 
+ const sharpen = new Discord.MessageEmbed()
+
+.setColor("RED")
+.setImage("https://media1.tenor.com/images/0c05631335594254143fb8beb56c7817/tenor.gif?itemid=11558347")
+.setTitle("EFSANE Bİ GOL ATTIN! ");
+
+  message.channel.send(sharpen);
+}; 
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["gol"],
+  permLevel: `Yok`
+};
+
+exports.help = {
+  name: "gol",
+  description: "Gol  atarsın",
+  usage: "gol"
+};
