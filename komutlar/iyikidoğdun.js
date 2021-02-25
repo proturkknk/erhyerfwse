0255bd5584901e0e15b5564c7b24d90f634c2206
Glitch (ukqzneglence) <none>
2021-02-25 08:38:49 +0000

    🎹🍚 Checkpoint ./komutlar/iyikidoğdun.js:26067249/890 ./komutlar/yardım.js:26067249/22

---
+++ komutlar/iyikidoğdun.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    const matador = new Discord.MessageEmbed()
+    .setAuthor('İyiki Doğdun <3 ')
+    .setColor('BLUE')
+    .setTimestamp()
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/R1dWfIYh0yz7usnkVc/giphy.gif`)
+    return message.channel.send(matador);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['İyikidoğdun','İyikidoğdun','İyikidoğdun'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'iyikidoğdun',
+  description: 'İyikidoğdun.',
+  usage: 'iyikidoğdun'
+};
