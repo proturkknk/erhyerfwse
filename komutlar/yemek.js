3d16ffd352e646d1471ee4c0bc6d4cfb0fc1650b
Glitch (sa-asd-hfsdhjfkjdsfdfkdopkewokdokf-d) <none>
2021-03-08 11:51:32 +0000

    🐃🏌 Checkpoint ./komutlar/yemek.js:26067249/824 ./komutlar/yardım.js:26067249/8

---
+++ komutlar/yemek.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    const matador = new Discord.MessageEmbed()
+    .setAuthor('yemeğin tadı güzel mi? :) ')
+    .setColor('BLUE')
+    .setTimestamp()
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/UnIlM9eoMwcOb89uk2/giphy.gif`)
+    return message.channel.send(matador);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['yemek','Yemek','YEMEK'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yemek',
+  description: 'yemek.',
+  usage: 'yemek'
+};
