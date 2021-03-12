7bbfc37d504e85d8d30ae24138320f42e2748e64
Glitch (sa-asd-hfsdhjfkjdsfdfkdopkewokdokf-d) <none>
2021-03-12 12:18:44 +0000

    🌷💕 Checkpoint ./komutlar/yılbaşı.js:26067249/1979

--- komutlar/yılbaşı.js
+++ komutlar/yılbaşı.js
@@ -1,38 +1,30 @@
 
-const Discord = require("discord.js")
+const Discord = require ("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
 
 exports.run = async (client, message, args) => {
-    function sayac(endtime){
-        var t = Date.parse(endtime) - Date.parse(new Date());
-        var saniye = Math.floor( (t/1000) % 60 );
-        var dakika = Math.floor( (t/1000/60) % 60 );
-        var saat = Math.floor( (t/(1000*60*60)) % 24 );
-        var gun = Math.floor( t/(1000*60*60*24) );
-        return {'toplam': t,'gun': gun, 'saat': saat, 'dakika': dakika, 'saniye': saniye };
-      };    
-      
-    var gn = new Date();
-    var son = 'January 1 ' + (gn.getFullYear() + 1) + " 00:00:00";
-    if (gn.getMonth() == 0 && gn.getDate() == 1) {
-        son = 'January 1 ' + (gn.getFullYear()) + " 00:00:00";
-    };
-        var t = sayac(son);
-        message.channel.send(new Discord.MessageEmbed().setDescription(`Gün: ${t.gun}, Saat: ${t.saat}, Dakika: ${t.dakika}, Saniye: ${t.saniye}`))
-        if(t.total<=0){
-            message.channel.send(new Discord.MessageEmbed().setDescription(`HAPPY NEW YEAR, NoName.txt`)) 
-        }
 
+  let yilbasi = new Date("2022-01-01 00:00:00");
+  let zaman = ms(yilbasi - Date.now());
+
+return message.channel.send(
+    `:label: **Yılbaşının kutlanmasına Dair Bilgiler Aşağıda;** \n\n> Gün: **${zaman.days}** \n> Saat: **${zaman.hours}** \n> Dakika: **${zaman.minutes}** \n\n:white_check_mark: **Yılbaşının kutlanmasına Bu Kadar Zaman Kaldı!**`
+  );
 };
 
-  
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  kategori: "eğlence",
+  permLevel: 0
+};
 
-  exports.help = {
-    name:"yılbaşı",
-    usage:"yılbaşı",
-    description:""
-    }
-    
-    exports.conf = {
-    aliases:[],
-    kategori:"eğlence"
-    }
+exports.help = {
+  name: "yılbaşı",
+
+  description:
+    "Yılbaşının kutlanmasına kaç gün kaç saat kaç dakika kaç saniye olduğunu gösterir.",
+  usage: ""
+};
