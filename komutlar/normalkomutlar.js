9f7e0c41b98d650f0880042ddfe669b2756a63b8
Glitch (sa-asd-hfsdhjfkjdsfdfkdopkewokdokf-d) <none>
2021-03-22 07:09:56 +0000

    🍜🍂 Checkpoint ./komutlar/normalkomutlar.js:26067249/7680

--- komutlar/normalkomutlar.js
+++ komutlar/normalkomutlar.js
@@ -1,23 +1,60 @@
-const Xaine = require('discord.js')
-exports.run = (client, message,args) => {
- const bok = new Xaine.MessageEmbed()
- .setAuthor("Xaine", client.user.avatarURL)
-  .setColor('BLACK')
-  .setTitle("Xaine Bot") 
-  .setDescription("[davet yazarak tüm davet linklerine ulaşabilirsiniz.]"(https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8%22)%22)
-.addField("-Normal komutlar (22)", ' avatar - davet - yılbaşı - nuke - hesapla - havadurumu - atatürk - is - bildir - korona - kilit - kilitaç - ping - saat - yapımcım - token - sunucubilgi - ekibimiz - mcskin - uyar - duyuru - sözlük')
-  .addField("-eğlence komutları  (57)", ' dondurma - xaine - atatürk - aşkölçer - balıktut - duello - efkarım - fbi - tersyazı - yazıtura - öp - aduketçek - ay - balık-tut - beşlik - dünya - ejderha-yazı - emojiyazı - espri - kartopu - kralol - slots - taksimdayı - tokat - yazı-tura - çıkma-teklifi - şekerye - düello - oylama - güneş - yıldız - kar - adamasmaca - stresçarkı - duygulandırıcı-söz - zarat - nsfw - gökyüzü - atatürk-sözleri - fal - boks-makinesi - şifre-oluştur - ağla - masketak - sevgili - asker - sayıtut - kolonya - kandil - komik - gol - iyikidoğdun - sarıl - yemek  bebek - şiir - çanakkalezaferi')
- .setImage(https://media.giphy.com/media/9CkIFgNLtPFAiiCqGN/giphy.gif)
-message.channel.send(bok)
-}
+const Discord = require ("discord.js");
+
+exports.run = (client, message) => {
+
+const EmbedFwhyCode = new Discord.MessageEmbed()
+
+.setColor("RANDOM")
+.setTitle("**▬▬▬▬▬▬[** ©️ **Eğlence Bot (V12)** ©️ **]▬▬▬▬▬▬**  \n\n> :floppy_disk: `+istatistik` **Botun istatistik bilgilerini açar.** \n> :floppy_disk: **Şuanda kullanılan prefix** `+`")
+.setThumbnail("https://cdn.discordapp.com/attachments/655459488236568597/655887650494087178/loading_1.gif")
+.setDescription(`
+
+▬▬▬▬▬▬[ :closed_lock_with_key: **Eğlence Komutları** :closed_lock_with_key: ]▬▬▬▬▬▬
+
+> **» +avatar :** Avatarınızı gösterir.
+> **» +davet :** Davet linklerini gösterir.
+> **» +yılbaşı :** yılbaşına kalan zamanı gösterir.
+> **» +nuke :** kanalı tekrar kurar ve tüm mesajları siler.
+> **» +hesapla :** matematik işl.
+> **» +ejderha-yazı :** Ejderha temalı logo üretirsiniz.
+> **» +espri :** Bot sizin için espri yapar.
+> **» +hesapla :** Belirttiğiniz matematik işlemini bot yapar.
+> **» +kartopu :** Etiketlediğiniz kişiye kartopu atarsınız.
+> **» +kaç-cm :** Bot malafat uzunluğunuzu söyler.
+> **» +kralol :** Kral olmanıza yarar.
+> **» +tokat :** Etiketlediğiniz kişiyi tokatlarsınız.
+> **» +yazıtura :** Bot ile yazı-tura oyununu oynarsınız.
+> **» +yılbaşı :** Yılbaşının gelmesine ne kadar yaklaştığınızı gösterir.
+> **» +öp :** Etiketlediğiniz kişiyi öpmenize yarar.
+> **» +şekerye :** Bot size şeker ikram eder.
+
+**▬▬▬▬▬▬▬[** :gear: **Bilgilendirme** :gear: **]▬▬▬▬▬▬▬**
+
+> :dizzy: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin!**
+> :open_file_folder: **Botun Destek Sunucusuna Gelmek İçin [Tıkla!](https://discord.gg/Kekc2pU)**
+> :round_pushpin: **Komut hakkında detaylı bilgi için: +yardım**
+> :airplane: **Aktif discord.js sürümüm: v12.2.0**
+> **Not! Botu sunucunuza eklemek istiyorsanız TREFAX#0038 e dm den yazabilirsiniz**
+**» Bağlantılar** 
+**[Destek Sunucusu](https://discord.gg/https://discord.gg/Kekc2pU)** **•** **[Botun Davet Linki](https://www.discord.com/)** **•** **[Web-Site](https://www.discord.com/)**
+`)
+ 
+.setFooter(client.user.username + "", client.user.avatarURL)
+.setTimestamp();
+
+return message.channel.send(EmbedFwhyCode)
+.then; //FwhyCode
+
+};
 exports.conf = {
-  enable: true, 
-  guildOnly: false, 
-  aliases: ['ym','Yardım','yardım','Yardim','yardim','Ym'], 
-  permLevel: 0 
-} 
-exports.help = {
-  name: "yardım", 
-  description: "Xaine V12 yardım", 
-  usage: "yardım" 
-}
+    enabled: true, 
+    guildOnly: false, 
+    aliases: [], 
+    permLevel: 0 
+};
+  
+  exports.help = {
+    name: 'yardım', 
+    description: 'The Help Command',
+    usage: 'help'
+};
