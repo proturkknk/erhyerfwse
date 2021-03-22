55161c9c6d8f40124017b2343b79c14acf722435
Glitch (sa-asd-hfsdhjfkjdsfdfkdopkewokdokf-d) <none>
2021-03-22 06:58:03 +0000

    🏖📠 Checkpoint ./komutlar/yardım.js:26067249/2757 ./komutlar/normalkomutlar.js:26067249/1564

---
+++ komutlar/normalkomutlar.js
@@ -0,0 +1,23 @@
+const Xaine = require('discord.js')
+exports.run = (client, message,args) => {
+ const bok = new Xaine.MessageEmbed()
+ .setAuthor("Xaine", client.user.avatarURL)
+  .setColor('BLACK')
+  .setTitle("Xaine Bot") 
+  .setDescription("[davet yazarak tüm davet linklerine ulaşabilirsiniz.]"(https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8%22)%22)
+.addField("-Normal komutlar (22)", ' avatar - davet - yılbaşı - nuke - hesapla - havadurumu - atatürk - is - bildir - korona - kilit - kilitaç - ping - saat - yapımcım - token - sunucubilgi - ekibimiz - mcskin - uyar - duyuru - sözlük')
+  .addField("-eğlence komutları  (57)", ' dondurma - xaine - atatürk - aşkölçer - balıktut - duello - efkarım - fbi - tersyazı - yazıtura - öp - aduketçek - ay - balık-tut - beşlik - dünya - ejderha-yazı - emojiyazı - espri - kartopu - kralol - slots - taksimdayı - tokat - yazı-tura - çıkma-teklifi - şekerye - düello - oylama - güneş - yıldız - kar - adamasmaca - stresçarkı - duygulandırıcı-söz - zarat - nsfw - gökyüzü - atatürk-sözleri - fal - boks-makinesi - şifre-oluştur - ağla - masketak - sevgili - asker - sayıtut - kolonya - kandil - komik - gol - iyikidoğdun - sarıl - yemek  bebek - şiir - çanakkalezaferi')
+ .setImage(https://media.giphy.com/media/9CkIFgNLtPFAiiCqGN/giphy.gif)
+message.channel.send(bok)
+}
+exports.conf = {
+  enable: true, 
+  guildOnly: false, 
+  aliases: ['ym','Yardım','yardım','Yardim','yardim','Ym'], 
+  permLevel: 0 
+} 
+exports.help = {
+  name: "yardım", 
+  description: "Xaine V12 yardım", 
+  usage: "yardım" 
+}
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,23 +1,34 @@
-const Xaine = require('discord.js')
-exports.run = (client, message,args) => {
- const bok = new Xaine.MessageEmbed()
- .setAuthor("Xaine", client.user.avatarURL)
-  .setColor('BLACK')
-  .setTitle("Xaine Bot") 
-  .setDescription("[davet yazarak tüm davet linklerine ulaşabilirsiniz.](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8%22)")
-.addField("-Normal komutlar (22)", '` avatar - davet - yılbaşı - nuke - hesapla - havadurumu - atatürk - is - bildir - korona - kilit - kilitaç - ping - saat - yapımcım - token - sunucubilgi - ekibimiz - mcskin - uyar - duyuru - sözlük`')
-  .addField("-eğlence komutları  (57)", '` dondurma - xaine - atatürk - aşkölçer - balıktut - duello - efkarım - fbi - tersyazı - yazıtura - öp - aduketçek - ay - balık-tut - beşlik - dünya - ejderha-yazı - emojiyazı - espri - kartopu - kralol - slots - taksimdayı - tokat - yazı-tura - çıkma-teklifi - şekerye - düello - oylama - güneş - yıldız - kar - adamasmaca - stresçarkı - duygulandırıcı-söz - zarat - nsfw - gökyüzü - atatürk-sözleri - fal - boks-makinesi - şifre-oluştur - ağla - masketak - sevgili - asker - sayıtut - kolonya - kandil - komik - gol - iyikidoğdun - sarıl - yemek  bebek - şiir - çanakkalezaferi`')
- .setImage(`https://media.giphy.com/media/9CkIFgNLtPFAiiCqGN/giphy.gif`)
-message.channel.send(bok)
-}
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+ const embed = new Discord.MessageEmbed()
+ .setAuthor(`${client.user.username} Yardım Menüsü`,client.user.avatarURL())
+ .setThumbnail(message.author.avatarURL())
+ .setColor('RANDOM')
+ .setDescription(`
+ > \`+moderasyon -> Moderasyon Komutlarını gösterir.\`
+
+ > \`+eğlence -> Eğlence Komutlarını gösterir.\`
+
+ > \`+kullanıcı -> Kullanıcı Komutlarını gösterir.\`
+ 
+ > \`+davet -> Botun Davetini ve Destek Sunucunu gösterir.\``)
+ .setFooter(`Xaine Bot Yardım Menüsü `)
+    .setTimestamp()
+    .setImage("https://media.giphy.com/media/9CkIFgNLtPFAiiCqGN/giphy.gif")
+message.channel.send(embed) 
+//sharpen
+};
+
 exports.conf = {
-  enable: true, 
-  guildOnly: false, 
-  aliases: ['ym','Yardım','yardım','Yardim','yardim','Ym'], 
-  permLevel: 0 
-} 
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
 exports.help = {
-  name: "yardım", 
-  description: "Xaine V12 yardım", 
-  usage: "yardım" 
-}
+  name: 'yardım',   //Xaine bot yardım menüsü
+  description: '',
+  usage: ''
+};
