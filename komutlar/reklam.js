a58c35c1b82591808ff7e9b1bfaa3663f9a8b7fb
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-03 06:49:09 +0000

    🛣🐀 Checkpoint ./komutlar/reklam.js:26067249/97

--- komutlar/reklam.js
+++ komutlar/reklam.js
@@ -11,8 +11,8 @@ if (!message.guild) {
     return message.author.send(ozelmesajuyari); }
   
   if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(` Bu komudu kullanabilmek için \`Sunucuyu Yönet\` yetkisine sahip olman gerek.`)
-  if (!args[0]) return message.channel.send(`:no_entry: Reklam Filtresini Ayarlamak İçin \`-reklam aç\` | Kapatmak İstiyorsanız \`-reklam kapat\` Yazabilirsiniz`)
-  if (args[0] !== 'aç' && args[0] !== 'kapat') return message.channel.send(`:no_entry: Reklam Filtresini Ayarlamak İçin \`-reklam aç\` | Kapatmak İstiyorsanız \`-reklam kapat\` Yazabilirsiniz`)
+  if (!args[0]) return message.channel.send(`:white_check_mark: Reklam Filtresini Ayarlamak İçin \`+reklam aç\` | Kapatmak İstiyorsanız \`+reklam kapat\` Yazabilirsiniz.`)
+  if (args[0] !== 'aç' && args[0] !== 'kapat') return message.channel.send(`:white_check_mark Reklam Filtresini Ayarlamak İçin \`+reklam aç\` | Kapatmak İstiyorsanız \`+reklam kapat\` Yazabilirsiniz.`)
 
     if (args[0] == 'aç') {
     db.set(`reklamFiltre_${message.guild.id}`, 'acik')
@@ -24,12 +24,12 @@ if (!message.guild) {
   if (args[0] == 'kapat') {
     
     let üye = await db.fetch(`reklamFiltre_${message.guild.id}`)
-    if (!üye) return message.channel.send(` \`Reklam filtresini açtığına emin misin?.\` `)
+    if (!üye) return message.channel.send(` \`Reklam filtresini açtığına emin misin?\` `)
     
     
     db.delete(`reklamFiltre_${message.guild.id}`)
     
-    message.channel.send(` \`Reklam Filtresini Kapattım.\` `)
+    message.channel.send(` \`Reklam Filtresi başarıyla kapatıldı!\` `)
   }
  
 };
