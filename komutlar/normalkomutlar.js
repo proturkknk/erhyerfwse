6b0507d5766b60f27a7143e9ac72c25d9ce8957e
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-05 11:22:39 +0000

    🎗🎣 Checkpoint ./komutlar/normal:26067249/83 ./komutlar/normalkomutlar.js:26067249/3337

--- komutlar/normalkomutlar.js
+++ komutlar/normalkomutlar.js
@@ -1,66 +1,22 @@
-const Discord = require ("discord.js");
-
-exports.run = (client, message) => {
-
-const EmbedFwhyCode = new Discord.MessageEmbed()
-
-.setColor("RANDOM")
-.setTitle("**▬▬▬▬▬▬[** ©️ **Eğlence Bot (V12)** ©️ **]▬▬▬▬▬▬**  \n\n> :floppy_disk: `+is` **Botun istatistik bilgilerini açar.** \n> :floppy_disk: **Şuanda kullanılan prefix** `+`")
-.setThumbnail("https://cdn.discordapp.com/attachments/655459488236568597/655887650494087178/loading_1.gif")
-.setDescription(`
-
-▬▬▬▬▬▬[ :closed_lock_with_key: **Xaine Bot Normal Komutlar** :closed_lock_with_key: ]▬▬▬▬▬▬
-
-> **» +avatar :** Avatarınızı gösterir.
-> **» +davet :** Davet linklerini gösterir.
-> **» +yılbaşı :** Yılbaşına kalan zamanı gösterir.
-> **» +hesapla :** Matematik işlemi hesaplarsınız.
-> **» +havadurumu :** Belirttiniz bölgenin hava durumuna bakarsınız.
-> **» +is :** Botun istatistik bilgilerine bakarsınız.
-> **» +bildir :** Şikayet-öneri de bulunursunuz.
-> **» +korona :** Belirttiniz ülkenin korona virüs bilgilerine bakarsınız (lütfen ingilizce olarak ülkeleri yazın.)
-> **» +ping :** Botun pingine bakarsınız.
-> **» +saat :** Saatin kaç olduğuna bakarsınız.
-> **» +kurucu :** Botun yapımcısına bakarsınız.
-> **» +token :** Botun tokenine bakarsınız :D.
-> **» +sunucubilgi :** Bulundunuz sunucu bilgilerini bot sizin için gösterir.
-> **» +sunucu-kur :** Bot sizin için sunucu kurar.
-> **» +uyar :** Belirttiniz kullanıcıyı uyarır.
-> **» +kullanıcıbilgi :** Belirttiniz kullanıcının bilgilerini gösterir.
-> **» +destek-ekibi :** Bulunduğunuz sunucuya botun **destek ekibi**ni  çağırır.
-> **» +ekibimiz :** Xaine Bot Ekibini gösterir.
-> **» +mcskin :** Belirttiniz ismin Minecraft skinine (karakter) bakarsınız.
-**» +sözlük :** Belirttiniz kelimenin sözlük anlamına bakarsınız.
-*» +kuruluş-tarihi :** Botun kuruluş tarihine bakarsınız.
-*» +toplam-komut :** Botun toplam kaç tane komutu olduğuna bakarsınız.
-*» +sunucukur :** Bot sizin için sunucu kurar.
-*» +çekiliş :** Çekiliş yaparsınız.
-**▬▬▬▬▬▬▬[** :gear: **Bilgilendirme** :gear: **]▬▬▬▬▬▬▬**
-
-> :dizzy: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin!**
-> :open_file_folder: **Botun Kod Paylaşım Ve Destek Sunucusuna Gelmek İçin [Tıkla!](https://discord.gg/Kekc2pU)**
-> :airplane: **Aktif discord.js sürümüm: v12.2.0**
-> :partying_face: ** Şikayet-öneride bulunmak isterseniz **bildir** komutunu kullanabilirsiniz!
-> :man_technologist: Yardıma ihtiyacınız olduğu zaman **destek-ekibi** komutunu kullanınız, destek ekibimiz size yardıma gelecektir.
-`)
-.setImage(`https://media.giphy.com/media/9CkIFgNLtPFAiiCqGN/giphy.gif`)
- 
-.setFooter(client.user.username + "", client.user.avatarURL)
-.setTimestamp();
-
-return message.channel.send(EmbedFwhyCode)
-.then; //xaine normal komutlar menüsü
-
-};
+const Xaine = require('discord.js')
+exports.run = (client, message,args) => {
+ const bok = new Xaine.MessageEmbed()
+ .setAuthor("Xaine", client.user.avatarURL)
+  .setColor('RANDOM')
+  .setTitle("Xaine Bot") 
+  .setDescription("[davet yazarak tüm davet linklerine ulaşabilirsiniz.](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8%22)%22")
+.addField("-Normal komutlar (22)", ' avatar - davet - yılbaşı - hesapla - havadurumu -  bildir - korona - ping - istatistik -  saat - yapımcım - token - sunucubilgi - ekibimiz - mcskin - uyar - duyuru - sözlük - kuruluş-tarihi - destek-ekibi - toplam-komut - çekiliş')
+ .setImage("https://media.giphy.com/media/9CkIFgNLtPFAiiCqGN/giphy.gif")
+message.channel.send(bok)
+}
 exports.conf = {
-    enabled: true, 
-    guildOnly: false, 
-    aliases: ['normal','Normal','NORMAL','normal-komutlar','Normal-komutlar','NORMAL-KOMUTLAR'], 
-    permLevel: 0 
-};
-  
-  exports.help = {
-    name: 'normal', 
-    description: 'Xaine Bot normal komutlar menüsü',
-    usage: 'Xaine Bot'
-};
+  enable: true, 
+  guildOnly: false, 
+  aliases: ['normal','normalkomutlar','Normal','Normalkomutlar','NORMALKOMUTLAR','NORMAL'], 
+  permLevel: 0 
+} 
+exports.help = {
+  name: "normal", 
+  description: "Xaine Bot v12 Normal Yardım Menüsü", 
+  usage: "normal" 
+}
