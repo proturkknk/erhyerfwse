ac18c4c529005fd2813886acdf4a168fb532cf96
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-05 07:56:04 +0000

    🦃🚪 Checkpoint ./bot.js:26067249/1131 ./komutlar/hatırlatıcı.js:26067249/1400

---
+++ komutlar/hatırlatıcı.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+const main = require("../ayarlar.json");
+var prefix = main.prefix;
+
+module.exports.run = async (Main, message, args) => {
+
+      let reminderTime = args[0];
+      if(!reminderTime) return message.channel.send(`** :warning: Yanlışlık var!**\n**Geçerli Kullanım Örneği:** ${prefix}hatırlat 10m uyandır \n(min = Dakika)`);
+
+      let Bilgi = new Discord.MessageEmbed()
+      .setColor('#ffffff')
+      .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({dynamic:true}))
+      .setDescription("**Hatırlatıcı Başarıyla Ayarlandı!**")
+      .setTimestamp()
+      message.channel.send(Bilgi);
+
+      let reminder = args.slice(1).join(" ");
+      
+      setTimeout(function(){
+        let Zamanlayıcı = new Discord.MessageEmbed()
+        .setColor('#ffffff')
+        .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({dynamic:true}))
+        .addField("Hatırlatıcı... zaman doldu!", `\`\`\`${reminder}\`\`\``)
+        .setTimestamp()
+        message.channel.send(Zamanlayıcı);
+      }, ms(reminderTime));
+
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['hatırlatıcı','Hatırlatıcı','HATIRLATICI'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'hatırlat'
+};
