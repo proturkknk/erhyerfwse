454a685a0119472b3edb21ee1785cc2b0c3d22c2
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-05 12:55:57 +0000

    🍡🍏 Checkpoint ./komutlar/normalkomutlar.js:26067249/7

--- komutlar/normalkomutlar.js
+++ komutlar/normalkomutlar.js
@@ -5,7 +5,7 @@ exports.run = (client, message,args) => {
   .setColor('RANDOM')
   .setTitle("Xaine Bot") 
   .setDescription("[davet yazarak tüm davet linklerine ulaşabilirsiniz.](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8%22)")
-.addField("-Normal komutlar (22)", ' avatar - davet - yılbaşı - hesapla - havadurumu -  bildir - korona - ping - istatistik -  saat - yapımcım - token - sunucubilgi - ekibimiz - mcskin - uyar - duyuru - sözlük - kuruluş-tarihi - destek-ekibi - toplam-komut - çekiliş - hatırlatıcı')
+.addField("-Normal komutlar (22)", ' avatar - davet - yılbaşı - hesapla - havadurumu -  bildir - korona - ping - istatistik -  saat - yapımcım - token - sunucubilgi - ekibimiz - mcskin - duyuru - sözlük - kuruluş-tarihi - destek-ekibi - toplam-komut - çekiliş - hatırlatıcı')
  .setImage("https://media.giphy.com/media/9CkIFgNLtPFAiiCqGN/giphy.gif")
 message.channel.send(bok)
 }
