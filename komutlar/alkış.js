cdb38564e519c6e5c839159b5bac2ad7fc9aaafb
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-09 10:46:25 +0000

    🐛🎄 Checkpoint ./komutlar/alkış.js:26067249/250

--- komutlar/alkış.js
+++ komutlar/alkış.js
@@ -3,9 +3,9 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
   
   const alkış = new Discord.MessageEmbed()
-  .setDescription("**Bravooo!!!**")
-  .setColor("#313131")
-  .setImage("https://thumbs.gfycat.com/WarpedAdmiredCormorant-size_restricted.gif")
+  .setDescription("**BRAVOOO!!!**")
+  .setColor("RANDOM")
+  .setImage("https://media.giphy.com/media/dKXjvcJU9pPEKnWufl/giphy.gif")
 
   message.channel.send(alkış);
 };
@@ -13,7 +13,7 @@ exports.run = function(client, message, args) {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['Alkış','alkış',],
+  aliases: ['Alkış','alkış','ALKIŞ'],
   permLevel: 0
 };
 
