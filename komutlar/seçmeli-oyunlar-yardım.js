3feedd612432a40d158044fbacf77777c36f19ef
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-15 10:03:40 +0000

    💑🛤 Checkpoint ./komutlar/seçmeli-oyunlar-yardım.js:26067249/3467

---
+++ komutlar/seçmeli-oyunlar-yardım.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message) => {
+  if (!message.guild) {
+    const ozelmesajuyari = new Discord.MessageEmbed()
+      .setColor()
+      .setTimestamp("RANDOM")
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .addField("**Komutlar Özel Mesajlarda Kullanılamaz!**");
+    return message.author.send(ozelmesajuyari);
+  }
+  const EmbedCrewCode = new Discord.MessageEmbed()
+
+    .setColor("RANDOM")
+    .setTitle("**  Xaine Bot  **")
+    .setThumbnail(
+      "https://cdn.discordapp.com/attachments/655459488236568597/655887650494087178/loading_1.gif"
+    ).setDescription(`
+
+**• Seçmeli oyunlar **
+
+
+> [karpuz]() → Karpuz oyununu oynarsınız.
+> [dondurma]() → Dondurma oyununu oynarsınız.
+> [çiftçi]() → Çiftçi oyununu oynarsınız.
+ 
+
+
+`);
+
+  return message.channel.send(EmbedCrewCode).then;
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["seçmeli-oyunlar-yardım","Seçmeli-oyunlar-yardım","SEÇMELİ-OYUNLAR-YARDIM"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "seçmeli-oyunlar-yardım",
+  description: "Botun Komut Listesini Gösterir!",
+  usage: "+seçmeli-oyunlar-yardım"
+};
