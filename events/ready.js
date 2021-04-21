8a785cce3c3e17cbf93154cc01a734008754ddf7
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-21 10:53:55 +0000

    ⚓️🎖 Checkpoint ./events/ready.js:26067249/283

--- events/ready.js
+++ events/ready.js
@@ -43,7 +43,8 @@ module.exports = client => {
       "Güncelleme2!: Yeni komutlarımız eklendi!",
       "✔ Yeni Moderasyon ve Çekiliş komutlarımıza göz attınız mı?",
       "En çok sevilen komutum: **dondurma**",
-      "Güncelleme3!: Yeni yardım menüleri!"
+      "Güncelleme3!: Yeni yardım menüleri!",
+      " ✔ İftar komutunu kullanarak belirttiniz şehrin iftar saatini öğrenebilceğinizi biliyor muydunuz?"
   ];
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
