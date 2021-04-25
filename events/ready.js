8d1f50cf273ae66879bc82ea47057d12fd1aa8ac
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-04-25 10:06:53 +0000

    👼👗 Checkpoint ./events/ready.js:26067249/91 ./komutlar/dünya-saatleri.js:26067249/4125

--- events/ready.js
+++ events/ready.js
@@ -45,9 +45,7 @@ module.exports = client => {
       "En çok sevilen komutum: **dondurma**",
       "Güncelleme3!: Yeni yardım menüleri!",
       " ✔ İftar komutunu kullanarak belirttiniz şehrin iftar saatini öğrenebilceğinizi biliyor muydunuz?",
-      "Sitemiz: https://xainebot.tr.gg/",
-      "Destek sunucumuzdaki çekilişe katılmayı unutmayın!",
-      "23 Nisan Kutlu Olsun!"
+      "Sitemiz: https://xainebot.tr.gg/"
   ];
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
