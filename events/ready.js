78f1539eec2b265dae0d2cd8d23dbc81bfd065ad
Glitch (xxxxxxxxxxxaa-w-e2340-2304-po32lk4k2l-3) <none>
2021-05-07 21:34:21 +0000

    🍼📯 Checkpoint ./events/ready.js:26067249/46

--- events/ready.js
+++ events/ready.js
@@ -46,7 +46,8 @@ module.exports = client => {
       "Güncelleme3!: Yeni yardım menüleri!",
       " ✔ İftar komutunu kullanarak belirttiniz şehrin iftar saatini öğrenebilceğinizi biliyor muydunuz?",
       "Sitemiz: http://www.xaine.tk/",
-      "Kod paylaşım sitemiz: https://xainecode.glitch.me/ "
+      "Kod paylaşım sitemiz: https://xainecode.glitch.me/ ",
+      "Kadir Gecemiz mübarek olsun! :) <3"
   ];
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
