36cb69e5fad58bfe7b263d1cb9a4c676ca3d9c2c
Glitch (xxxalsogkfgpewje--fkgwpkefi-sal-fi-dskfkdsflkdgkfd) <none>
2021-05-10 09:30:28 +0000

    🐴🍕 Checkpoint ./komutlar/bayram.js:26067249/827

---
+++ komutlar/bayram.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    const matador = new Discord.MessageEmbed()
+    .setAuthor('İyi bayramlar! 🍬')
+    .setColor('RANDOM')
+    .setTimestamp()
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/GmPcTlXQY11w83nwLf/giphy.gif`)
+    return message.channel.send(matador);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bayram','Bayram','BAYRAM'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'bayram',
+  description: 'bayram.',
+  usage: 'bayram'
+};
