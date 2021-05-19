5dbe602c530526c3680e40086727b79450292bb9
Glitch (xxxalsogkfgpewje--fkgwpkefi-sal-fi-dskfkdsflkdgkfd) <none>
2021-05-19 11:09:44 +0000

    🎾💑 Checkpoint ./komutlar/19mayıs.js:26067249/837

---
+++ komutlar/19mayıs.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    const matador = new Discord.MessageEmbed()
+    .setAuthor('19 Mayıs Atatürkü')
+    .setColor('RANDOM')
+    .setTimestamp()
+    .setDescription('')
+        .setImage(`https://media.giphy.com/media/JTDLcjTwIKpcEzsABn/giphy.gif`)
+    return message.channel.send(matador);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['19mayıs','19Mayıs','19MAYIS'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: '19mayıs',
+  description: '19mayıs.',
+  usage: '19mayıs'
+};
