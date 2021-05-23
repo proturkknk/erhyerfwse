191de75eeced2e38d1f2cf3f7a37fc470b215790
Glitch (xxxalsogkfgpewje--fkgwpkefi-sal-fi-dskfkdsflkdgkfd) <none>
2021-05-23 13:03:17 +0000

    🙌🚟 Checkpoint ./komutlar/kahve.js:26067249/289 ./komutlar/seçmeli-oyunlar-yardım.js:26067249/153 ./komutlar/yardım.js:26067249/7

--- komutlar/kahve.js
+++ komutlar/kahve.js
@@ -3,10 +3,12 @@ const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message, params) => {
     const matador = new Discord.MessageEmbed()
-    .setAuthor('Bu komut şuan yapım aşamasında daha sonra tekrar deneyin. Tahmini bitiş tarihi: 24.05.2021')
+    .setAuthor('Bu komut şuan yapım aşamasında, daha sonra tekrar deneyin. Tahmini bitiş tarihi: 24.05.2021')
     .setColor('RANDOM')
     .setTimestamp()
     .setDescription('')
+        .setImage(`https://media.giphy.com/media/TFCQtHTYpUk3YWkCZK/giphy.gif`)
+    return message.channel.send(matador);
     };
 
 exports.conf = {
