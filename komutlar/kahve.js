d735695579e1db637256c509500ef2457d7d14fd
Glitch (xxxalsogkfgpewje--fkgwpkefi-sal-fi-dskfkdsflkdgkfd) <none>
2021-05-24 10:09:41 +0000

    🐬🐙 Checkpoint ./komutlar/kahve.js:26067249/8462

--- komutlar/kahve.js
+++ komutlar/kahve.js
@@ -1,25 +1,205 @@
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const settings = require('../ayarlar.json');
+const prefix = settings.prefix;
+const db = require('quick.db')
+const Discord = require('discord.js')
 
-exports.run = (client, message, params) => {
-    const matador = new Discord.MessageEmbed()
-    .setAuthor('Bu komut şuan yapım aşamasında, daha sonra tekrar deneyin. Tahmini bitiş tarihi: 24.05.2021')
-    .setColor('RANDOM')
+exports.run = async(client, msg, args) => {
+  
+  let cooldown = true
+  
+  if(cooldown) {
+    cooldown = false
+    
+    const liste = await db.fetch('list');
+    
+  if(liste) {
+    if(liste.find(o => o.id === msg.author.id)) {
+      
+      const kullanıcı = liste.find(o => o.id === msg.author.id)
+      const yasaklayan = kullanıcı.yasaklayan
+      const sebep = kullanıcı.sebep
+      
+      const embed = new Discord.MessageEmbed()
+      .setTitle('Karaliste')
+      .setDescription('Neden Karalistem Var?')
+      .setThumbnail(msg.author.avatarURL)
+      .addField('Karaliste Bilgiler','\nYasaklayan: **'+yasaklayan+'**\nSebep: '+sebep)
+      .setColor('RED')
+      .setTimestamp()
+      
+      msg.channel.send(embed)
+      return
+    }
+  }
+    const antispam = await db.fetch(`anti_${msg.channel.id}`)
+    db.set(`anti_${msg.channel.id}`,'open')
+    var filter = m => m.author.id == msg.author.id;
+    const ilk = new Discord.MessageEmbed()
+    .setTitle('Xaine Cafe ye hoşgeldiniz. Kahve mi almak istiyorsunuz?')
+    .setColor('BLUE')
+    .setThumbnail(msg.author.avatarURL)
+    .addField('Kahve almak ister misiniz?','   [evet]   [hayır]')/////////////////////////////////////////////////:D
     .setTimestamp()
-    .setDescription('')
-        .setImage(`https://media.giphy.com/media/TFCQtHTYpUk3YWkCZK/giphy.gif`)
-    return message.channel.send(matador);
-    };
+    msg.channel.send(ilk).then(msj => {
+      msg.channel.awaitMessages(filter,{
+      max: 1,
+      time: 6000,
+      errors: ['time']
+    }).catch(err => {
+        //hiçbişi
+      })
+        .then(cvp => {
+        if(!cvp) {
+          msg.reply('**Malesef süreniz doldu! :(**')
+          db.delete(`anti_${msg.channel.id}`)
+          return
+        }
+        if(cvp.first().content.toLowerCase() != 'hayır' && cvp.first().content.toLowerCase() != 'evet') {
+          msg.reply('tamam.')
+          db.delete(`anti_${msg.channel.id}`)
+          return
+        }
+      if(cvp.first().content.toLowerCase() == 'evet') {
+        const dvm = new Discord.MessageEmbed()
+        .setTitle('Lütfen bekleyin...')
+        .setColor('#ffbe19')
+        .setDescription('Kahvenizin bardağı hazırlanıyor...')///
+        .setThumbnail(msg.author.avatarURL)
+        .setTimestamp()
+        msg.channel.send(dvm)
+        
+        setTimeout(function() {
+          const com = new Discord.MessageEmbed()
+        .setTitle('Lütfen bir seçenek seçin.')
+        .setColor('GREEN')
+        .setThumbnail(msg.author.avatarURL)
+          .addField('Kahveniz mocha kahvesi olsunmu?','[evet] [hayır]')
+        .setTimestamp()
+        msg.channel.send(com)
+          .then(msj => {
+          msg.channel.awaitMessages(filter,{
+            max: 1,
+            time: 6000,
+            errors: ['time']
+          }).catch(err => {})
+            .then(cvp => {
+            if(!cvp) {
+          msg.reply('**Süren Doldu!**')
+          db.delete(`anti_${msg.channel.id}`)
+          return
+        }
+        if(cvp.first().content.toLowerCase() != 'hayır' && cvp.first().content.toLowerCase() != 'evet') {
+          msg.reply('malesef sadece mocha kahvemiz var.')
+          db.delete(`anti_${msg.channel.id}`)
+          return
+        }
+            if(cvp.first().content.toLowerCase() == 'evet') {
+              const indiriyo = new Discord.MessageEmbed()
+            .setTitle('Lütfen bekleyin...')
+            .setDescription('Kahveniz yapılıyor...')
+            .setColor('#ffbe19')
+            .setTimestamp()
+            .setThumbnail(msg.author.avatarURL)
+            msg.channel.send(indiriyo)
+              setTimeout(function() {
+                const virus = new Discord.MessageEmbed()
+            .setTitle('Olamaz! Kahvenizi yolda giderken düşürdünüz!')
+            .setColor('RED')
+                .addField('Kahveniz yere düştü!','[iç] [kutusunu at]')
+            .setTimestamp()
+            .setThumbnail(msg.author.avatarURL)
+            msg.channel.send(virus).then(msj => {
+              msg.channel.awaitMessages(filter,{
+                max: 1,
+                time: 6000,
+                errors: ['time']
+              }).catch(err => {})
+                .then(cvp => {
+                if(!cvp) {
+                  msg.reply('**Süren Doldu!**')
+                  db.delete(`anti_${msg.channel.id}`)
+                  return
+                }
+                if(cvp.first().content.toLowerCase() != 'ye' && cvp.first().content.toLowerCase() != 'ye ' && cvp.first().content.toLowerCase() != 'Çöpe at') {
+                  msg.reply('Kahvenizin kutusu çöpe atıldı ve sizin için başka bir mocha kahve hazırlandı Afiyet olsun!')
+                  db.delete(`anti_${msg.channel.id}`)
+                  return
+                }
+                if(cvp.first().content.toLowerCase() == 'çöpe at') {
+                  const sil = new Discord.MessageEmbed()
+            .setTitle('Afiyet olsun!')
+            .setDescription('Kahvenizin kutusu çöpe atıldı ve sizin için başka bir mocha kahve hazırlandı Afiyet olsun!')
+            .setColor('RED')
+            .setTimestamp()
+            .setThumbnail(msg.author.avatarURL)
+            msg.channel.send(sil)
+            db.delete(`anti_${msg.channel.id}`)
+            return
+                }
+                if(cvp.first().content.toLowerCase() == '' || cvp.first().content.toLowerCase() == 'iç') {
+                  const bsvr = new Discord.MessageEmbed()
+            .setTitle('ZEHİRLENDİN!')
+            .setDescription('Yere düşen kahveyi içtiniz için zehirlendiniz!')
+            .setColor('RED')
+            .setTimestamp()
+            .setThumbnail(msg.author.avatarURL)
+            msg.channel.send(bsvr)
+            db.delete(`anti_${msg.channel.id}`)
+            return
+                }
+              })
+            })
+              },5000)
+            }
+            
+            if(cvp.first().content.toLowerCase() == 'hayır') {
+              const gg = new Discord.MessageEmbed()
+            .setTitle('Başka yok!')
+            .setDescription('Başka çeşit dondurmamız yok.')
+            .setColor('RED')
+            .setTimestamp()
+            .setThumbnail(msg.author.avatarURL)
+            msg.channel.send(gg)
+            db.delete(`anti_${msg.channel.id}`)
+            return
+            }
+          })
+        })
+        },2000)
+        
+        db.delete(`anti_${msg.channel.id}`)
+      }
+      if(cvp.first().content.toLowerCase() == 'hayır') {
+            const banane = new Discord.MessageEmbed()
+            .setTitle('Almazsan alma')
+            .setDescription('Dondurma almadınız.')
+            .setColor('RED')
+            .setTimestamp()
+            .setThumbnail(msg.author.avatarURL)
+            msg.channel.send(banane)
+        db.delete(`anti_${msg.channel.id}`)
+      }
+    })
+    })
+    
+    
+    setTimeout(function() {
+       cooldown = true
+      }, 2500);
+  }
+  
+    
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['kahve','Kahve','KAHVE'],
+  aliases: ['dondurma','DONDURMA','DONDURMA','Dondurma','dondurma'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'kahve',
-  description: 'kahve.',
-  usage: 'kahve'
+  name: 'dondurma',
+  description: 'dondurma',
+  usage: 'dondurma'
 };
