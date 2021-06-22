422f96097222795c1324ee8f1b8aacc5121d8a8c
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-06-22 10:13:49 +0000

    🏓🌱 Checkpoint ./komutlar/isim-değiştir.js:26067249/134

--- komutlar/isim-değiştir.js
+++ komutlar/isim-değiştir.js
@@ -1,4 +1,4 @@
-c000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000onst Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const ayarlar = require('../ayarlar.json');
 const prefix = ayarlar.prefix
