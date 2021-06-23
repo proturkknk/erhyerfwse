7332c2cf06782b4d3b98bd28036490cae4d32dc5
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-06-23 19:18:09 +0000

    🎷🙆 Checkpoint ./komutlar/ping.js:26067249/5

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -4,7 +4,7 @@ exports.run = async (client ,message, args) =>{
 
   
   const embed = new Discord.MessageEmbed()
-  .setTitle(" <:hourglass:825313852321169429> Pingim aşağıda yazmaktadır")
+  .setTitle(" <:hourglass:825313852321169429> Pingim aşağıda yazmaktadır.")
   .setColor("RANDOM")
   .setDescription(`${client.ws.ping} ms`)
   message.channel.send(embed)
