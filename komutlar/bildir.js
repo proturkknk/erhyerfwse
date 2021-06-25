5420cd7a627fd3735d223e914242faf0cd94b071
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-06-25 14:35:37 +0000

    👳🍢 Checkpoint ./komutlar/bildir.js:26067249/244

--- komutlar/bildir.js
+++ komutlar/bildir.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args) => {
 
   
 let istek = args.slice(0).join(' ')
-if(!istek) return message.channel.send('**şikayet veya öneri için +bildir (şikayet veya öneriniz)** ')
+if(!istek) return message.channel.send('**Bildirmek istedinizi bildirmek için: +bildir (Şikayet - Hata bildirimi - Öneri - Vb.)** Örnek: +bildir balık-tut komutunda hata var.')
 
 const embed = new Discord.MessageEmbed()
 .setTitle("Xaine bildirme Sistemi")
@@ -11,7 +11,7 @@ const embed = new Discord.MessageEmbed()
 .setDescription(`**Bug Kanalı** ${message.channel.name} \n **Bug Bildirilen Sunucu** \`${message.guild.name}\` \n **Bugu Bildiren Kullanıcı** <@${message.author.id}> \n **Bildirilen Bug :** \`${istek}\``)
 client.channels.cache.get('787987649977384960').send(embed)
   
-message.channel.send("Bildiriminiz, ekibimize gönderildi, İyi günler dileriz. :white_check_mark:").then(message => message.delete({ timeout: 5000 }));
+message.channel.send("Bildiriminiz, ekibimize gönderildi! İyi günler dileriz. :white_check_mark:").then(message => message.delete({ timeout: 5000 }));
 };
 exports.conf = {
   enabled: true,
