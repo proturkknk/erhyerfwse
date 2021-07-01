bcea9f91afb83e2b57e916f38dec5fc52d4dac0a
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-07-01 14:01:57 +0000

    🕋🚌 Checkpoint ./komutlar/spam.js:26067249/924 ./komutlar/spam-engel.js:26067249/1831

---
+++ komutlar/spam-engel.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+    const matador = new Discord.MessageEmbed()
+    .setAuthor('Bu komut şuan yapım aşamasında, daha sonra tekrar deneyin. Tahmini bitiş tarihi: 02.07.2021')
+    .setColor('RANDOM')
+    .setTimestamp()
+    .setDescription('')
+        .setImage('https://media.giphy.com/media/TFCQtHTYpUk3YWkCZK/giphy.gif')
+    return message.channel.send(matador);
+    };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['SPAM-ENGEL','spam-engel','Spam-engel'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'spam-engel',
+  description: 'spam-engel.',
+  usage: 'spam-engel'
+};
