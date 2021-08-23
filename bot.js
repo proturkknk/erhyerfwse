cdfd61142386c3970f51c3e5ac857a66285d3d04
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-08-23 14:50:05 +0000

    ✨🚥 Checkpoint ./bot.js:26067249/36 ./views/index.ejs:26067249/34

--- bot.js
+++ bot.js
@@ -331,9 +331,8 @@ client.on("guildMemberAdd", async member => {
 //Muteliyken s
 client.on("guildCreate", guild => {
   let kanal = guild.channels.cache.filter(c => c.type === "text").random();
-
   kanal.send(
-    "Selam! Beni Sunucunuza Eklediğiniz İçin Teşekkür ederim! Sizlere en iyi şekilde hizmet etmeye çalışıcağım. Bu arada! Destek sunucumuza gelmek ister misiniz? Çeşitli duyurular ve çekilişler buradan yapılıyor. Bot bir hata yaşadığında veya yenilik geldiğinde destek sunucusundan bildiriliyor. Ve sunucumuzda hergün yapılan çekilişler etkinlikler bile var! Gelmek istersen yardım menüsünün hemen altına davet linkini bulabilirsiniz. İyi eğlenceler!"
+    "Selam! Ben Xaine! Beni Sunucunuza Eklediğiniz İçin Teşekkür ederim! Sizlere en iyi şekilde hizmet etmeye çalışıcağım. Bu arada! Destek sunucumuza gelmek ister misiniz? Çeşitli duyurular ve çekilişler buradan yapılıyor. Bot bir hata yaşadığında veya yenilik geldiğinde destek sunucusundan bildiriliyor. Ve sunucumuzda hergün yapılan çekilişler etkinlikler bile var! Gelmek istersen yardım menüsünün hemen altına davet linkini bulabilirsiniz. İyi eğlenceler!"
   );
 });
 
