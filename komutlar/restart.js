f43ccb82f2986427063d23736365ba2ffa89424f
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-09-01 15:25:58 +0000

    🌁🚏 Checkpoint ./.glitch-assets:26067249/551 ./komutlar/restart.js:26067249/2997

---
+++ komutlar/restart.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const ayarlar = require("../ayarlar.json")
+
+module.exports.run = async (bot, message, args) => {
+    if(message.author.id !== "696365117063036986") if(message.author.id !== "522834911732695041") return message.channel.send(":no_entry_sign: Geliştiricilerimizden Biri Değilsin. Bu Komutu kullanamazsın!")
+    
+    message.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
+    console.log(`BOT: Bot yeniden başlatılıyor...`);
+    process.exit(0);
+  })
+    
+          
+}
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["Restart","restart","yenile","yeniden başlat"],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: 'restart',
+  description: 'Sistemi yeniden başlatır',
+  usage: 'restart'
+};
