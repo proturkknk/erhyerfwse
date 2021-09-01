9640bd3da8ee3acdb81c953be7e07ee6ed25757a
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-09-01 15:36:59 +0000

    🐩🙈 Checkpoint ./komutlar/restart.js:26067249/15

--- komutlar/restart.js
+++ komutlar/restart.js
@@ -5,7 +5,7 @@ const ayarlar = require("../ayarlar.json")
 module.exports.run = async (bot, message, args) => {
     if(message.author.id !== "696365117063036986") if(message.author.id !== "522834911732695041") return message.channel.send(":no_entry_sign: Geliştiricilerimizden Biri Değilsin. Bu Komutu kullanamazsın!")
     
-    message.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
+    message.channel.send(`Bot yeniden başlatılıyor...`).then(msg => {
     console.log(`BOT: Bot yeniden başlatılıyor...`);
     process.exit(0);
   })
