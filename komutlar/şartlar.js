9709edd03b6767ca1e4af13469e1005d706945c0
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-09-03 08:26:12 +0000

    🏮🏯 Checkpoint ./komutlar/şartlar.js:26067249/4902

--- komutlar/şartlar.js
+++ komutlar/şartlar.js
@@ -1,22 +1,42 @@
 const Discord = require("discord.js");
-const db = require("quick.db")
-exports.run = (client, message, args) => {
-  let şartkabul = db.fetch(`şartkabul${message.author.id}`)
-    if (şartkabul) return;
 
-   return  message.channel.send ("Şartlarımız:\n\n 1- Sunucunuzda Xaine Botun ismini hakaret, küfür ve benzeri isimler koymayınız. \n 2-Xaine Botu asla kötü niyet içeren şeylerle kullanmayınız. \n 3- Bota, kullanıcıları, ekibine ve kurucusuna hakaret, küfür, kötü söz söylemek kesinlikle yasaktır! \n 4- Botun kopyasını veya ismi ve pp(profil fotoğrafı) olan botlar oluşturmayın! **Şartlarımız bu kadardı. Şartlarımızı kabul edip uymayan kullanıcılarımız botu kullanamayacak ve bot sunucularından atılıcaktır.")
-.setColor('RANDOM')
+exports.run = (client, message) => {
+  if (!message.guild) {
+    const ozelmesajuyari = new Discord.MessageEmbed()
+      .setColor('RANDOM')
+      .setTimestamp("RANDOM")
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .addField("**Komutları Özel Mesajlarda Kullanılamaz!**");
+    return message.author.send(ozelmesajuyari);
+  }
+  const EmbedCrewCode = new Discord.MessageEmbed()
+
+    .setColor("RANDOM")
+    .setTitle("**  Xaine Bot **")
+    .setThumbnail(
+      "https://media.giphy.com/media/vPJdwN8Hsu6K4hczUE/giphy.gif"
+    ).setDescription(`
+
+**•Xaine Botu kullanmak için şartlarımız**
+
+> Şartlarımız:\n\n 1- Sunucunuzda Xaine Botun ismini hakaret, küfür ve benzeri isimler koymayınız. \n 2-Xaine Botu asla kötü niyet içeren şeylerle kullanmayınız. \n 3- Bota, kullanıcıları, ekibine ve kurucusuna hakaret, küfür, kötü söz söylemek kesinlikle yasaktır! \n 4- Botun kopyasını veya ismi ve pp(profil fotoğrafı) olan botlar oluşturmayın! 
+**Şartlarımız bu kadardı. Şartlarımızı kabul edip uymayan kullanıcılarımız botu kullanamayacak ve bot sunucularından atılıcaktır.**
+
+**Şartları kabul ediyorsanız +kabulet yazınız.**
+
+`);
+
+  return message.channel.send(EmbedCrewCode).then;
 };
 exports.conf = {
   enabled: true,
-  guildOnly: true,
-  permLevel: 0,
-  kategori: "bot",
-  aliases: ['şartlar','Şartlar','Sartlar','sartlar','ŞARTLAR']
+  guildOnly: false,
+  aliases: ["şartlar","Şartlar","ŞARTLAR"],
+  permLevel: 0
 };
 
 exports.help = {
   name: "şartlar",
-  description: "Şartları kabul edersiniz. C:",
-  usage: ""
+  description: "Xaine Bot kullanmak için şartlar",
+  usage: "+şartlar"
 };
