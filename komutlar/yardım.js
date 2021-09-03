83c7016618b03b4a52097366a2bb69db42793378
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-09-03 08:14:57 +0000

    🌏⛳️ Checkpoint ./komutlar/yardım.js:26067249/80 ./komutlar/şartlar.js:26067249/256

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -27,22 +27,22 @@ Yeni **sürüm** komutu eklendi!
 
 > :gear: | +ayarlamalı-komutlar -> Ayarlamalı komutları gösterir.
 
- > ⭐| +aldığımız-tedbirler -> Xaine Bot ve sizin için aldığımız tedbirleri gösterir.
+ > :man_police_officer: | +aldığımız-tedbirler -> Xaine Bot ve sizin için aldığımız tedbirleri gösterir.
  
- > ⭐| +seçmeli-oyunlar -> Seçmeli oyunlar yardım menüsünü gösterir.
+ > ⭐ | +seçmeli-oyunlar -> Seçmeli oyunlar yardım menüsünü gösterir.
  
- > ⭐| +logo-yardım -> Logo yardım menüsünü gösterir.
+ > :ringed_planet: | +logo-yardım -> Logo yardım menüsünü gösterir.
  
- > ⭐| +gif-yardım -> Gif yardım menüsünü gösterir.
+ > :rosette: | +gif-yardım -> Gif yardım menüsünü gösterir.
  
- > ⭐| +davet -> Botla ilgili tüm linkleri gösterir.
+ > :tickets: | +davet -> Botla ilgili tüm linkleri gösterir.
  
-  > ⭐+Bakımdaki-komutlar -> Botun bakımdaki komutlarını gösterir.`)
+  > :wrench: | +Bakımdaki-komutlar -> Botun bakımdaki komutlarını gösterir.`)
  
  .setFooter(`Xaine Bot Yardım Menüsü `)
 .addField("Bağlantılar","[Davet et](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8)\n[Destek sunucusu](https://discord.gg/99C4tGzgK4)\n[Oy verme linki](https://top.gg/bot/774235071653216286/vote)")
  .setFooter(`© 2020 Xaine Bot | Tüm hakları saklıdır`)
-.addField(`Duyuru!`,`Hey selam! Ben Xaine | Destek sunucumuza katılmak ister misiniz? Destek sunucumuzda çekilişler, bot hakkında duyurular ve güncellemeleri öğrenebilirsiniz! Katılmak isterseniz üsteki **bağlantılar** bölümünden **destek sunucusu** yazan yere tıklayabilirsiniz.`)
+.addField(`Duyuru!`,`Hey selam! Ben Xaine | Destek sunucumuza katılmak ister misiniz? Destek sunucumuzda çekilişler, bot hakkında duyurular ve güncellemeleri öğrenebilirsiniz! Katılmak isterseniz üsteki **bağlantılar** bölümünden **Destek sunucusu** yazan yere tıklayabilirsiniz.`)
     .setTimestamp()
     .setImage("https://media.giphy.com/media/YKqCL6KfuCJOdh2d8p/giphy.gif")
 message.channel.send(embed) 
--- komutlar/şartlar.js
+++ komutlar/şartlar.js
@@ -3,7 +3,9 @@ const db = require("quick.db")
 exports.run = (client, message, args) => {
   let şartkabul = db.fetch(`şartkabul${message.author.id}`)
     if (şartkabul) return;
-    return message.channel.send("Şartlarımız:\n\n 1- Sunucunuzda Xaine Botun ismini hakaret, küfür ve benzeri isimler koymayınız. \n 2-Xaine Botu asla kötü niyet içeren şeylerle kullanmayınız. \n 3- Bota, kullanıcıları, ekibine ve kurucusuna hakaret, küfür, kötü söz söylemek kesinlikle yasaktır! \n 4- Botun kopyasını veya ismi ve pp(profil fotoğrafı) olan botlar oluşturmayın! **Şartlarımız bu kadardı. Şartlarımızı kabul edip uymayan kullanıcılarımız botu kullanamayacak ve bot sunucularından atılıcaktır.")
+
+   return  message.channel.send ("Şartlarımız:\n\n 1- Sunucunuzda Xaine Botun ismini hakaret, küfür ve benzeri isimler koymayınız. \n 2-Xaine Botu asla kötü niyet içeren şeylerle kullanmayınız. \n 3- Bota, kullanıcıları, ekibine ve kurucusuna hakaret, küfür, kötü söz söylemek kesinlikle yasaktır! \n 4- Botun kopyasını veya ismi ve pp(profil fotoğrafı) olan botlar oluşturmayın! **Şartlarımız bu kadardı. Şartlarımızı kabul edip uymayan kullanıcılarımız botu kullanamayacak ve bot sunucularından atılıcaktır.")
+.setColor('RANDOM')
 };
 exports.conf = {
   enabled: true,
