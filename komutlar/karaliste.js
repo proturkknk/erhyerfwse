da698f4e57a677bed2aa3ac559f3e99c08da3bc5
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-10-02 13:49:05 +0000

    🙌🎇 Checkpoint ./komutlar/karaliste.js:26067249/2143 ./events/message.js:26067249/1586

--- komutlar/karaliste.js
+++ komutlar/karaliste.js
@@ -5,18 +5,19 @@ exports.run = (client, message, args) => {
 const black = args[0]
 if(message.author.id !== ayarlar.sahip) return message.channel.send(
     new Discord.MessageEmbed()
-    .setDescription(`**Malesef bu komutu kullanamazsınız! Sahibim değilsiniz.**`)
+    .setDescription(`**Malesef!, Sahibim Değilsin!**`)
 //Bots For List Yapımı!
 )
 if(!black) return message.channel.send(
     new Discord.MessageEmbed()
     .setDescription(`Üyenin İDsini Yaz`)
 )//Bots For List Yapımı!
-if(black == ayarlar.sahip) return message.channel.send(`Kendini karalisteye ekleyemezsiniz`)
+if(black == ayarlar.sahip) return message.channel.send(`Kanka Kendini BlackListe Alamazsın Anla :D`)
 message.channel.send(
     new Discord.MessageEmbed()
     .setDescription(`**<@${black}> Adlı Kişi BlackList'e Eklendi!**`)
 )
+db.set(`karaliste.${black}`, black)
 }
 
 exports.conf = {
