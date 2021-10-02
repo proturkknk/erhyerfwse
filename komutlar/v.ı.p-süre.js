85af44040bad83948fa1dca3deda6b2039320b45
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-10-02 07:58:49 +0000

    🐸👨‍❤️‍👨 Checkpoint ./komutlar/v.ı.p-üye.js:26067249/1602 ./komutlar/v.ı.p-süre.js:26067249/721

---
+++ komutlar/v.ı.p-süre.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const db = require("quick.db")
+const ms = require("ms")
+exports.run = async (client, message, args) => {
+let sürem = await db.fetch(`gold.${message.member.id}.goldsüre`)
+if(!sürem) return message.reply("Sen V.I.P üye değilsin.")
+message.channel.send("Süren:"+(ms(sürem-message.createdTimestamp)).replace("s", "sn").replace("m", "dk").replace("h", "sa").replace("d", "g"))
+}
+//made by ceyhun
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'vıp-sürem',
+    description: 'vıp-sürem',
+    usage: 'vıp-sürem'
+}
---
+++ komutlar/v.ı.p-üye.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+const db = require("quick.db")
+const ms = require("ms")
+exports.run = async (client, message, args) => {
+module.exports.run = async (bot, message, args) => {
+    if(message.author.id !== "696365117063036986") return message.channel.send(":no_entry_sign: Geliştiricilerimizden Biri Değilsin. Bu Komutu kullanamazsın!")
+let a = message.mentions.users.first() || args[0]
+if(!a) return message.reply("Yetkili, Lütfen birini etiketle veya bir id gir.")
+if(message.mentions.users.first()) {
+let b = args[1]
+if(!b) return message.reply("Yetkili, Lütfen bir zaman aralığı gir.")
+db.set(`gold.${a.id}.goldsüre`, message.createdTimestamp+(ms(b.replace("sn", "s").replace("dk", "m").replace("sa", "h").replace("g", "d"))))
+message.reply("İşlem başarılı, Kullanıcı artık V.I.P üye!")
+return;
+} else {
+let b2 = args[1]
+if(!b2) return message.reply("Yetkili, Lütfen bir zaman aralığı gir.")
+db.set(`gold.${a}.goldsüre`, message.createdTimestamp+(ms(b2.replace("sn", "s").replace("dk", "m").replace("sa", "h").replace("g", "d"))))
+message.reply("İşlem başarılı!")
+return;
+}
+//made by ceyhun
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0 // botsahibi permine ayarlıyoruz*
+};
+
+exports.help = }
+    name: 'vıp-yap',
+    description: 'vıp-yap',
+    usage: 'vıp-yap'
+{
