0c74ba7d7c69716e33cb0bc64c44ac908bfbb388
Glitch (xa-gjjg-epeejr-ka--fpep--gkglv-jdflk-wlaje-irn-tkl) <none>
2021-10-03 20:20:37 +0000

    🐝🙌 Checkpoint ./komutlar/sürüm.js:26067249/17

--- komutlar/sürüm.js
+++ komutlar/sürüm.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
   const rexus = new Discord.MessageEmbed()
-    .setColor("#00ee00")
-    .setDescription("Şuanda kullanılan Xaine Bot sürümü: 3.6")
+    .setColor("RANDOM")
+    .setDescription("Şuanda kullanılan Xaine Bot sürümü: 3.8")
     
   message.channel.send(rexus);
 };
