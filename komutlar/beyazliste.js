9b1bd2a380408ad017136226d62433f67b9034d9
Glitch (xa-gjjg-epeejr-ka-afpep--gkglva-jdflk-wlajirn-tkl) <none>
2021-12-23 18:11:54 +0000

    💡😎 Checkpoint ./komutlar/beyazliste.js:26067249/22

--- komutlar/beyazliste.js
+++ komutlar/beyazliste.js
@@ -1,6 +1,6 @@
 const {MessageEmbed} = require('discord.js')
 const ayarlar = require('../ayarlar.json')
-const { Database } = require('quick.db')//Seninki quick.db İse Bunu Sil Onun yerine const db = require('quick.db') Yaz
+const db = require('quick.db')//Seninki quick.db İse Bunu Sil Onun yerine const db = require('quick.db') Yaz
 exports.run = (client, message, args) => {
 const white = args[0]
 if(message.author.id !== ayarlar.sahip) return message.channel.send(
