b5c3d8583f38aa4e258bbca0a5d8c987849094d1
Glitch (xa-gjjg-epeejr-ka-afpep--gkglva-jdflk-wlajirn-tkl) <none>
2022-01-20 18:16:48 +0000

    ⛰🐿 Checkpoint ./views/index.ejs:26067249/547 ./bot.js:25692167/3 ./events/ready.js:26067249/12

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ passport.use(
       clientID: "774235071653216286",
       clientSecret: process.env.SECRET,
       callbackURL:
-        "https://www.xaine.tk/login",
+        "https://www.xainebot.tk/login",
       scope: scopes
     },
     function(accessToken, refreshToken, profile, done) {
