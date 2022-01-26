39c8b4b1580d9fc96264a3490113edce8fe53441
Glitch (xa-gjjg-epeejr-ka-afpep--gkglva-jdflk-wlajirn-tkl) <none>
2022-01-26 14:37:21 +0000

    ✈️🏊🏼 Checkpoint ./komutlar/normalkomutlar.js:26067249/29 ./komutlar/duygulandırıcı-sözler.js:26067249/304

--- komutlar/normalkomutlar.js
+++ komutlar/normalkomutlar.js
@@ -5,7 +5,7 @@ exports.run = (client, message,args) => {
   .setColor('RANDOM')
   .setTitle("Xaine Bot") 
   .setDescription("[ <:civcivkalp:853213881111150642>  davet yazarak tüm botla ilgili  linklere ulaşabilirsiniz. Yada buraya tıklayarak botu davet edebilirsiniz.](https://discord.com/oauth2/authorize?client_id=774235071653216286&scope=bot&permissions=8)")
-.addField("-Normal komutlar (24)", ' avatar - sürüm - çeviri - kullanıcı-bilgi - bot-bilgi - roblox-oyuncu-bilgi - steam-oyun - davet - Atatürk-sözleri - duygulandırıcı-sözler - yılbaşı - hesapla - hava-durumu -  bildir - korona - ping - istatistik -  saat - yapımcım - token - sunucubilgi - sunucukur - ekibimiz - mcskin - duyuru - sözlük - kuruluş-tarihi - destek-ekibi - toplam-komut - çekiliş(bakımda) - hatırlatıcı - yetkilerim - iftar - yenilikler')
+.addField("-Normal komutlar (24)", ' avatar - sürüm - çeviri - kullanıcı-bilgi - bot-bilgi -roblox-oyuncu-bilgi  davet - Atatürk-sözleri - duygulandırıcı-sözler - yılbaşı - hesapla - hava-durumu -  bildir - korona - ping - istatistik -  saat - yapımcım - token - sunucubilgi - sunucukur - ekibimiz - mcskin - duyuru - sözlük - kuruluş-tarihi - destek-ekibi - toplam-komut - çekiliş(bakımda) - hatırlatıcı - yetkilerim - iftar - yenilikler')
  .setImage("https://media.giphy.com/media/YKqCL6KfuCJOdh2d8p/giphy.gif")
 message.channel.send({embeds: [bok]})
 }
