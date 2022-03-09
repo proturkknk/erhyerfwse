b643a14d129ddefd925e07e62bfeb15c8d0779c4
Glitch (xa-gjjg-epeejr-ka-afpep--gkglva-jdflk-wlajirn-tkl) <none>
2022-03-09 16:55:47 +0000

    💓🕶 Checkpoint ./komutlar/şiir.js:26067249/11

--- komutlar/şiir.js
+++ komutlar/şiir.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
         .setImage(replies[result]);
     message.channel.send({embeds: [gifembed]});
 };
-//CodeShare
+
 exports.conf = {
   enabled: true,
   guildOnly: false,
