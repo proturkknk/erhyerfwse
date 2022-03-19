2993f389c0a1d2bf1a5b90adf855d0909e4ae9dc
Glitch (xa-gjjg-epeejr-ka-afpep--gkglva-jdflk-wlajirn-tkl) <none>
2022-03-19 15:56:36 +0000

    📼☺️ Checkpoint ./komutlar/iftar-zamanı.js:26067249/1995 ./komutlar/ramazan.js:26067249/116

--- komutlar/iftar-zamanı.js
+++ komutlar/iftar-zamanı.js
@@ -3,23 +3,23 @@ const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message, params) => {
     const matador = new MessageEmbed()
-    .setAuthor('Hayırlı ramazanlar! ⛪')
+    .setAuthor('Hayırlı İftarlar! 😸')
     .setColor('RANDOM')
     .setTimestamp()
     .setDescription('')
-        .setImage(`https://media.giphy.com/media/12llaake5RvPvwjMhl/giphy.gif`)
+        .setImage(`https://media.giphy.com/media/zEOGKLj9YnrwqeCw8G/giphy.gif`)
     return message.channel.send({embeds: [matador]});
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['ramazan','Ramazan','RAMAZAN'],
+  aliases: ['iftar-zamanı','İftar-zamanı','İFTAR-ZAMANI'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'ramazan',
+  name: 'iftar-zamanı',
   description: 'ramazan.',
   usage: 'ramazan'
 };
