ca6165c25772cb47c652b3c5ff9ced36da7a1023
Glitch (xa-gjjg-epeejr-ka-afpep--gkglva-jdflk-wlajirn-tkl) <none>
2022-03-19 08:10:33 +0000

    🎟🐌 Checkpoint ./komutlar/ban.js:26067249/3270

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,50 +1,44 @@
-const {MessageEmbed} = require("discord.js");
-const fs = require("fs");
+const Discord = require('discord.js');
+
 
 exports.run = (client, message, args) => {
   if (!message.guild) {
-    const ozelmesajuyari = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTimestamp()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .addField(" :warning: Uyarı!", "`ban` adlı komutu özel mesajlarda kullanamazsın.");
-    return message.author.send({embeds: [ozelmesajuyari]});
-  }
-  let guild = message.guild;
-  let reason = args.slice(1).join(" ");
-  let dızcılaraselam = message.mentions.users.first();
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`ban` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
+  let guild = message.guild
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = guild.channels.find('isim', 'mod-log');
+  if (!modlog) return message.reply('`:warning: Hata!: mod-log` kanalını bulamıyorum.');
+  if (reason.length < 1) return message.reply(':warning: Ban sebebini yazmalısınız.');
+  if (message.mentions.users.size < 1) return message.reply(':warning: Kimi banlayacağımı yazmalısın.').catch(console.error);
 
-  if (message.mentions.users.size < 1)
-    return message.channel
-      .send(`Lütfen sunucudan yasaklayacağınız kişiyi etiketleyin.`)
-      .catch(console.error);
+  if (!message.guild.member(user).bannable) return message.reply(':warning: Yetkilileri banlama yetkim yok.');
+  message.guild.ban(user, 2);
 
-  message.guild.members.cache.get
-    return message.channel.send(
-      `❌ Belirttiğiniz kişinin Yetkisi Benden Daha Üstün. O yüzden Banlayamıyorum.(Yasaklama)`
-    );
-  message.guild.member(dızcılaraselam).ban();
-
-  message.channel.send(
-    " Başarılı! Belirttiğiniz kullanıcıyı banlandım. " +
-      dızcılaraselam +
-      " İD'li kullanıcı **" +
-      reason +
-      "** sebebiyle sunucudan yasaklandı."
-  );
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .addField('Eylem:', 'Ban')
+    .addField('Kullanıcı:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Yetkili:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Sebep', reason);
+  return guild.channels.get(modlog.id).sendEmbed(embed);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['ban','Ban','BAN'],
-  permLevel: 3,
-  kategori: "moderasyon"
+  aliases: [],
+  permLevel: 2
 };
 
 exports.help = {
-  name: "ban",
-  category: "moderasyon",
-  description: "İstediğiniz kişiyi sunucudan yasaklar. Xaine Bota ait! eğer alt yapıya girdiyseniz çalarsanız hakkımız sonuna kadar haramdır.",
-  usage: "ban <@kullanıcı> <sebep>"
+  name: 'ban',
+  description: 'İstediğiniz kişiyi banlar.',
+  usage: 'ban [kullanıcı] [sebep]'
 };
